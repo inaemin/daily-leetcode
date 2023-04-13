ebe65e97a6cad4a5b0ce8cbfd77f72bcb2724005
inaemin <97337038+inaemin@users.noreply.github.com>
2023-04-14 01:06:15 +0900

    Time: 75 ms (19.92%) | Memory: 43.7 MB (80.86%) - LeetSync

--- validate-stack-sequences/validate-stack-sequences.js
+++ validate-stack-sequences/validate-stack-sequences.js
@@ -4,21 +4,16 @@
  * @return {boolean}
  */
 var validateStackSequences = function(pushed, popped) {
-    let answer = true;
-    const start_idx = pushed.indexOf(popped[0])
-    let left_pushed = pushed.slice(0, start_idx+1)
-    let right_pushed = pushed.slice(start_idx+1)
-    for (let el of popped) {
-        if (left_pushed[left_pushed.length-1] === el) left_pushed.pop()
-        else if (right_pushed.indexOf(el) !== -1) {
-            const slice_idx = right_pushed.indexOf(el);
-            left_pushed = [...left_pushed, ...right_pushed.slice(0, slice_idx)]
-            right_pushed = right_pushed.slice(slice_idx+1)
-        } else {
-            answer = false;
-            return answer;
-        }
-    }
+    const stack = []
+    let i=0;
 
-    return answer;
+    pushed.forEach((el) => {
+        stack.push(el)
+        while (stack.length && stack[stack.length-1] === popped[i]) {
+            stack.pop()
+            i += 1;
+        }
+    })
+
+    return popped.length === i;
 };
