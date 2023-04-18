ea8c4455d61073f8b7dd53ad796aacc4d679e1af
inaemin <97337038+inaemin@users.noreply.github.com>
2023-04-18 13:18:43 +0900

    Time: 57 ms (70.46%) | Memory: 41.5 MB (96.21%) - LeetSync

--- merge-strings-alternately/merge-strings-alternately.js
+++ merge-strings-alternately/merge-strings-alternately.js
@@ -5,12 +5,10 @@
  */
 var mergeAlternately = function(word1, word2) {
     let i=0;
-    const w1 = word1.split("")
-    const w2 = word2.split("")
     const result = []
-    while (w1[i] || w2[i]) {
-        if (w1[i]) result.push(w1[i])
-        if (w2[i]) result.push(w2[i])
+    while (word1[i] || word2[i]) {
+        if (word1[i]) result.push(word1[i])
+        if (word2[i]) result.push(word2[i])
         i += 1;
     }
     return result.join("")
