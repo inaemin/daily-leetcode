c2e84fe220dd123051d395db359dafa92f75f1ad
inaemin <97337038+inaemin@users.noreply.github.com>
2023-04-19 19:46:56 +0900

    Time: 59 ms (81.46%) | Memory: 43.8 MB (90.00%) - LeetSync

--- keys-and-rooms/keys-and-rooms.js
+++ keys-and-rooms/keys-and-rooms.js
@@ -3,14 +3,29 @@
  * @return {boolean}
  */
 var canVisitAllRooms = function(rooms) {
-    const visited = new Set([0])
-    const queue = rooms[0];
-    while (queue.length) {
-        visited.add(queue[0])
-        rooms[queue[0]].forEach((el) => {
-            if (!visited.has(el)) queue.push(el);
+    // BFS using queue
+    // const visited = new Set([0])
+    // const queue = rooms[0];
+    // while (queue.length) {
+    //     visited.add(queue[0])
+    //     rooms[queue[0]].forEach((el) => {
+    //         if (!visited.has(el)) queue.push(el);
+    //     })
+    //     queue.shift()
+    // }
+    // return visited.size === rooms.length;
+
+    // DFS using stack
+    const visited = new Set([0]);
+    const stack = rooms[0];
+    while (stack.length) {
+        const key = stack[stack.length-1]
+        visited.add(key)
+        stack.pop();
+        rooms[key].forEach((el) => {
+            if (!visited.has(el)) stack.push(el)
         })
-        queue.shift()
     }
+
     return visited.size === rooms.length;
 };
