434bffd4d8c0e529828515779514608cce4051da
inaemin <97337038+inaemin@users.noreply.github.com>
2023-04-21 19:26:07 +0900

    Time: 56 ms (90.25%) | Memory: 42.4 MB (98.01%) - LeetSync

--- subsets/subsets.js
+++ subsets/subsets.js
@@ -3,17 +3,22 @@
  * @return {number[][]}
  */
 var subsets = function(nums) {
+    // using recursion
     const result = []
     const bt = (arr, idx) => {
         if (idx === nums.length) {
-            result.push(arr);
+            result.push([...arr]);
             return;
         }
+        arr.push(nums[idx]);
         bt(arr, idx+1);
-        bt([...arr, nums[idx]], idx+1)
+        arr.pop();
+        bt(arr, idx+1);
     }
 
     bt([], 0) // currentLetter, nextIdx
 
     return result;
+
+    // using iteration
 };
