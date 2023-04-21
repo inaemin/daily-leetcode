74e85c66a850a80e2bb004744e847edf3a1a7801
inaemin <97337038+inaemin@users.noreply.github.com>
2023-04-21 20:53:37 +0900

    Time: 93 ms (7.88%) | Memory: 45.3 MB (29.52%) - LeetSync

--- permutations/permutations.js
+++ permutations/permutations.js
@@ -4,35 +4,36 @@
  */
 var permute = function(nums) {
     // recursion1
-    // const result = [];
-    // const bt = (subset, idx, rest) => {
-    //     if (!rest.length) {
-    //         result.push([...subset])
-    //         return;
-    //     }
+    const result = [];
+    const bt = (subset, idx, rest) => {
+        if (rest.length === 1) {
+            result.push([...subset, rest[0]])
+            return;
+        }
 
-    //     rest.forEach((fixed, i, o) => {
-    //         subset.push(fixed);
-    //         bt(subset, idx+1, o.filter(el => el !== fixed));
-    //         subset.pop();
-    //     })
-    // }
+        rest.forEach((fixed, i, o) => {
+            subset.push(fixed);
+            bt(subset, idx+1, o.filter(el => el !== fixed));
+            subset.pop();
+        })
+    }
 
-    // bt([], 0, nums) //current, nextIdx
-    // return result;
+    bt([], 0, nums) //current, nextIdx
+    return result;
+
 
     // recursion2
-    const result = [];
-    if (nums.length === 1) return nums.map(el => [el]);
+    // const result = [];
+    // if (nums.length === 1) return nums.map(el => [el]);
 
-    nums.forEach((fixed, idx, org) => {
-        const rest = org.filter(el => el !== fixed);
-        const permutations = permute(rest);
-        const attach = permutations.map(el => [fixed, ...el]);
-        result.push(...attach);
-    })
+    // nums.forEach((fixed, idx, org) => {
+    //     const rest = org.filter(el => el !== fixed);
+    //     const permutations = permute(rest);
+    //     const attach = permutations.map(el => [fixed, ...el]);
+    //     result.push(...attach);
+    // })
 
-    return result;
+    // return result;
 
     // iteration
 };
