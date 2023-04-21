4909a4c0df5f6d2bf06873fa02e5a1078fda795c
inaemin <97337038+inaemin@users.noreply.github.com>
2023-04-21 19:46:36 +0900

    Time: 59 ms (80.79%) | Memory: 44.1 MB (43.86%) - LeetSync

--- subsets/subsets.js
+++ subsets/subsets.js
@@ -4,21 +4,31 @@
  */
 var subsets = function(nums) {
     // using recursion
-    const result = []
-    const bt = (arr, idx) => {
-        if (idx === nums.length) {
-            result.push([...arr]);
-            return;
+    // const result = []
+    // const bt = (arr, idx) => {
+    //     if (idx === nums.length) {
+    //         result.push([...arr]);
+    //         return;
+    //     }
+    //     arr.push(nums[idx]);
+    //     bt(arr, idx+1);
+    //     arr.pop();
+    //     bt(arr, idx+1);
+    // }
+
+    // bt([], 0) // currentLetter, nextIdx
+    
+    // return result;
+
+    // using iteration -> doesn't have stackoverflow problem.
+    const result = [[]];
+    for (let i=0; i<nums.length; i++) {
+        const n = nums[i];
+        const len = result.length;
+        for (let j=0; j<len; j++) {
+            result.push([...result[j], n])
         }
-        arr.push(nums[idx]);
-        bt(arr, idx+1);
-        arr.pop();
-        bt(arr, idx+1);
     }
 
-    bt([], 0) // currentLetter, nextIdx
-
     return result;
-
-    // using iteration
 };
