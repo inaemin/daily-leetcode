50c1f06922e88e817aaf9670eee666e4b13c7dc5
inaemin <97337038+inaemin@users.noreply.github.com>
2023-04-22 23:37:45 +0900

    Time: 62 ms (91.46%) | Memory: 45.6 MB (66.17%) - LeetSync

---
+++ diameter-of-binary-tree/diameter-of-binary-tree.js
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var diameterOfBinaryTree = function(root) {
+    let diameter = 0;
+    const depth = (node) => {
+        if (!node) return 0;
+
+        const left = depth(node.left);
+        const right = depth(node.right);
+        // implementation
+        diameter = Math.max(diameter, left+right)
+
+        return Math.max(left, right) + 1// 리턴값은 자식노드의 depth값.
+    }
+
+    depth(root)
+    return diameter;
+};
+
+
+
+
+
+    // let diameter = 0;
+    // const depth = (node) => {
+    //     if (!node) return 0
+    //     let left = depth(node.left);
+    //     let right = depth(node.right);
+    //     diameter = Math.max(diameter, left+right);
+    //     return Math.max(left, right) + 1; //Math.max(left, right) 가 depth를 의미.
+    // }
+    // depth(root)
+    // return diameter;
