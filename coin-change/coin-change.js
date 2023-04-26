c06e0e62098cedf7073321a3a077541f63cd007d
inaemin <97337038+inaemin@users.noreply.github.com>
2023-04-26 17:12:41 +0900

    Time: 101 ms (80.27%) | Memory: 47.6 MB (53.10%) - LeetSync

--- coin-change/coin-change.js
+++ coin-change/coin-change.js
@@ -6,15 +6,13 @@
 var coinChange = function(coins, amount) {
     const dp = Array(amount+1).fill(Infinity)
     dp[0] = 0
-    // set 1 at every coin.
-    coins.forEach(el => {
-        dp[el] = 1;
-    })
 
-    // F(n) = Math.min(F(n-2), F(n-3), F(n-5)) + 1;
     for (let i=1; i<=amount; i++) {
-        let min = coins.map(el => dp[i-el]).filter(el => el !== undefined);
-        if (min.length > 0) dp[i] = Math.min(...min) + 1;
+        for (let j=0; j<coins.length; j++) {
+            if (coins[j] <= i) {
+                dp[i] = Math.min(dp[i], dp[i-coins[j]] + 1);
+            }
+        }
     }
 
     return dp[amount] === Infinity ? -1 : dp[amount];
