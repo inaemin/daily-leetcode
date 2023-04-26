e39381b8b6e5d39b3e0550391f58d1275b047a99
inaemin <97337038+inaemin@users.noreply.github.com>
2023-04-26 23:24:46 +0900

    Time: 54 ms (93.85%) | Memory: 42.7 MB (47.93%) - LeetSync

--- decode-ways/decode-ways.js
+++ decode-ways/decode-ways.js
@@ -3,39 +3,23 @@
  * @return {number}
  */
 var numDecodings = function(s) {
-    let answer;
-    const s_arr = [...s].reduce((r, e) => {
-        // 문자열에 0이 있는 경우, 무조건 앞 원소가 있어야 하고, 합쳤을 때 1과 26 사이에 있는 수여야 함.
-        if (e === "0") {
-            let last_num = `${r[r.length-1]}0`;
-            if (last_num === "10" || last_num === "20") {
-                r.pop();
-                r.push(`${r[r.length-1]}0`);
-            } else {
-                // 아닌 경우에는 
-                answer = -1;
-            }
-        } else r.push(e);
-        return r;
-    }, [])
+    const isValid = (s) => {
+        return Number(s) >= 10 && Number(s) <= 26;
+    }
 
-    if (answer === -1) return 0;
-    
-    const dp = [];
-    s_arr.reverse();
-    for (let i=0; i<s_arr.length; i++) {
-        if (i === 0) dp.push(1);
+    const len = s.length;
+    const dp = new Array(len).fill(0);
+    for (let i=len-1; i>=0; i--) {
+        if (s[i] === "0") continue;
+        if (s[i+1] === "0") {
+            if (isValid(s.slice(i, i+2))) dp[i] = dp[i+2] === undefined ? 1 : dp[i+2];
+            else return 0;
+        }
         else {
-            // s_arr[i] -> 통과 -> dp[i-1]
-            // s_arr[i]와 s_arr[i-1] -> 통과 -> dp[i-2]
-            let p = dp[i-1];
-            let double_digit = Number(`${s_arr[i]}${s_arr[i-1]}`);
-            if (double_digit >= 1 && double_digit <= 26) {
-                p += dp[i-2] ? dp[i-2] : 1;
-            }
-            dp.push(p);
+            dp[i] += dp[i+1] === undefined ? 1 : dp[i+1]
+            if (isValid(s.slice(i, i+2))) dp[i] += dp[i+2] === undefined ? 1 : dp[i+2]
         }
     }
 
-    return dp[dp.length-1];
+    return dp[0];
 };
