26a4f898e1d546a9c78db694f96d90bf87021e6e
inaemin <97337038+inaemin@users.noreply.github.com>
2023-04-26 15:58:32 +0900

    Time: 68 ms (55.15%) | Memory: 44.8 MB (29.42%) - LeetSync

--- minimum-path-sum/minimum-path-sum.js
+++ minimum-path-sum/minimum-path-sum.js
@@ -3,20 +3,21 @@
  * @return {number}
  */
 var minPathSum = function(grid) {
-    const m = grid.length
-    const n = grid[0].length
-    for (let i=0; i<m; i++) {
-        for (let j=0; j<n; j++) {
-            if (i === 0 && j === 0) continue;
-            else if (i === 0) {
-                grid[i][j] += grid[i][j-1];
-            } else if (j === 0) {
-                grid[i][j] += grid[i-1][j];
+    // (m+1)*(n+1) grid filled with 0;
+    const m = grid.length;
+    const n = grid[0].length;
+    const dp = [...Array(m+1)].map(el => new Array(n+1).fill(0));
+    for (let i=1; i<=m; i++) {
+        for (let j=1; j<=n; j++) {
+            if (i === 1) {
+                dp[i][j] = grid[i-1][j-1] + dp[i][j-1]
+            } else if (j === 1) {
+                dp[i][j] = grid[i-1][j-1] + dp[i-1][j];
             } else {
-                grid[i][j] += Math.min(grid[i-1][j], grid[i][j-1]);
+                // dp[i][j] = grid[i-1][j-1] + Math.min(dp[i-1][j], dp[i][j-1])
+                dp[i][j] = grid[i-1][j-1] + Math.min(dp[i-1][j], dp[i][j-1])
             }
         }
     }
-
-    return grid[m-1][n-1];
+    return dp[m][n];
 };
