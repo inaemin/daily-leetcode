14568959890aaa868080bd68d9a14773c3dad601
inaemin <97337038+inaemin@users.noreply.github.com>
2023-04-26 15:28:36 +0900

    Time: 68 ms (40.02%) | Memory: 44.3 MB (27.19%) - LeetSync

--- min-cost-climbing-stairs/min-cost-climbing-stairs.js
+++ min-cost-climbing-stairs/min-cost-climbing-stairs.js
@@ -4,9 +4,10 @@
  */
 var minCostClimbingStairs = function(cost) {
     const res = [0, 0];
-    for (let i=2; i<=cost.length; i++) {
+    let top = cost.length;
+    for (let i=2; i<=top; i++) {
         res.push(Math.min(res[i-2]+cost[i-2], res[i-1]+cost[i-1]));
     }
 
-    return res.at(-1);
+    return res[top];
 };
