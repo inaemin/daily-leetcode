6190056246d255ea1fcd6f418178035b79a30a30
inaemin <97337038+inaemin@users.noreply.github.com>
2023-04-27 19:32:51 +0900

    Time: 74 ms (14.26%) | Memory: 43 MB (28.82%) - LeetSync

--- longest-common-prefix/longest-common-prefix.js
+++ longest-common-prefix/longest-common-prefix.js
@@ -3,15 +3,15 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-    if (strs.length === 1) return strs[0];
-    let prefix = ""
-    for (let i=0; i<strs[0].length; i++) {
-        let s = strs[0][i];
-        for (let j=1; j<strs.length; j++) {
-            let ss = strs[j][i];
-            if (s !== ss) return prefix
+    if (!strs.length) return "";
+    let prefix = strs[0];
+    for (let i=1; i<strs.length; i++) {
+        if (strs[i].indexOf(prefix) !== 0) {
+            prefix = prefix.slice(0, -1);
+            i = 0;
         }
-        prefix += s;
+        if (prefix === "") return prefix
     }
+
     return prefix;
 };
