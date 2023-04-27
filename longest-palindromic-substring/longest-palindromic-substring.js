744d6f0e13ca03e3cd36bb9cc4303881fa28690c
inaemin <97337038+inaemin@users.noreply.github.com>
2023-04-27 22:11:38 +0900

    Time: 872 ms (21.31%) | Memory: 88.7 MB (5.22%) - LeetSync

--- longest-palindromic-substring/longest-palindromic-substring.js
+++ longest-palindromic-substring/longest-palindromic-substring.js
@@ -4,28 +4,24 @@
  */
 var longestPalindrome = function(s) {
     const dp = [...Array(s.length)].map(el => new Array(s.length).fill(0));
+    let max_i=0, max_j=0, max=0;
     for (let c=0; c<s.length; c++) {
         for (let i=0; i<s.length; i++) {
             if (c === 0) dp[i][i+c] = 1;
             else if (c === 1 && i+c < s.length) {
                 dp[i][i+c] = s[i] === s[i+c] ? 2 : 0;
             }
-            else if (i+c < s.length && s[i] === s[i+c]) {
-                dp[i][i+c] = dp[i+1][i+c-1] !== 0 ? dp[i+1][i+c-1] + 2 : 0;
+            else if (i+c < s.length && s[i] === s[i+c] && dp[i+1][i+c-1] !== 0) {
+                dp[i][i+c] = dp[i+1][i+c-1] + 2;
+            }
+
+            if (dp[i][i+c] > max) {
+                max_i = i;
+                max_j = i+c;
+                max = dp[i][i+c];
             }
         }
     }
-
-    let max_i=0, max_j=0, max=0;
-    dp.forEach((e, m) => {
-        e.forEach((e, n) => {
-            if (dp[m][n] > max) {
-                max_i = m;
-                max_j = n;
-                max = dp[m][n];
-            } 
-        })
-    })
 
     return s.slice(max_i, max_j+1);    
 };
