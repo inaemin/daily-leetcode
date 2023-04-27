4e8ad013a93fed4e6ac1c41aef32e1c3c5b6e3fd
inaemin <97337038+inaemin@users.noreply.github.com>
2023-04-27 22:16:15 +0900

    Time: 860 ms (21.66%) | Memory: 88.6 MB (5.22%) - LeetSync

--- longest-palindromic-substring/longest-palindromic-substring.js
+++ longest-palindromic-substring/longest-palindromic-substring.js
@@ -8,12 +8,8 @@ var longestPalindrome = function(s) {
     for (let c=0; c<s.length; c++) {
         for (let i=0; i<s.length; i++) {
             if (c === 0) dp[i][i+c] = 1;
-            else if (c === 1 && i+c < s.length) {
-                dp[i][i+c] = s[i] === s[i+c] ? 2 : 0;
-            }
-            else if (i+c < s.length && s[i] === s[i+c] && dp[i+1][i+c-1] !== 0) {
-                dp[i][i+c] = dp[i+1][i+c-1] + 2;
-            }
+            else if (c === 1 && i+c < s.length && s[i] === s[i+c]) dp[i][i+c] = 2;
+            else if (i+c < s.length && s[i] === s[i+c] && dp[i+1][i+c-1] !== 0) dp[i][i+c] = dp[i+1][i+c-1] + 2;
 
             if (dp[i][i+c] > max) {
                 max_i = i;
