5068e96d97ba6768fc1893fc802ebdb3f2337824
inaemin <97337038+inaemin@users.noreply.github.com>
2023-04-29 05:34:53 +0900

    Time: 362 ms (8.70%) | Memory: 45.4 MB (100.00%) - LeetSync

--- similar-string-groups/similar-string-groups.js
+++ similar-string-groups/similar-string-groups.js
@@ -8,37 +8,23 @@ var numSimilarGroups = function(strs) {
       for (let i=0; i<s1.length; i++) {
         if (s1[i] !== s2[i]) cnt += 1;
       }
-      return cnt === 0 || cnt === 2 ? true : false;
+      return cnt === 0 || cnt === 2;
     }
-    const vertices = [...Array(strs.length)].map(el => new Array());
-    const edges = [];
-    for (let i=0; i<strs.length; i++) {
-      for (let j=i; j<strs.length; j++) {
-        if (isSimilar(strs[i], strs[j])) edges.push([i, j])
-      }
-    }
-    // graph 만들기
-    for (let edge of edges) {
-      const [x, y] = edge;
-      vertices[x].push(y);
-      vertices[y].push(x);
-    }
-    
+
     let group = 0;
     const visited = new Set();
-    const stack = [];
-    const dfs = (i) => {
-      if (visited.has(i)) return;
-      visited.add(i);
-      stack.push(...vertices[i]);
-      while (stack.length) {
-        let next = stack.pop()
-        dfs(next)
+    const dfs = (str) => {
+      if (visited.has(str)) return;
+      visited.add(str);
+      for (let next of strs) {
+        if (isSimilar(str, next)) {
+          dfs(next)
+        }
       }
     }
-    for (let i=0; i<vertices.length; i++) {
-      if (!visited.has(i)) {
-        dfs(i);
+    for (let str of strs) {
+      if (!visited.has(str)) {
+        dfs(str)
         group += 1;
       }
     }
