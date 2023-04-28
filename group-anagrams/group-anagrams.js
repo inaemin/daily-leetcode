7fefff099a158fbe20a16e8e072711895d330f6c
inaemin <97337038+inaemin@users.noreply.github.com>
2023-04-28 18:17:24 +0900

    Time: 118 ms (85.58%) | Memory: 52.5 MB (89.46%) - LeetSync

--- group-anagrams/group-anagrams.js
+++ group-anagrams/group-anagrams.js
@@ -4,22 +4,11 @@
  */
 var groupAnagrams = function(strs) {
     const obj = new Map();
-    const countStr = (s) => {
-        const alphabet_table = new Map();
-        'abcdefghijklmnopqrstuvwxyz'.split("").forEach(el => alphabet_table.set(el, 1));
-        s.split("").forEach(el => alphabet_table.set(el, alphabet_table.get(el) + 1));
-
-        let answer = "";
-        for (let e of alphabet_table.keys()) {
-            if (alphabet_table.get(e) !== 0) answer += `${e}${alphabet_table.get(e)}`
-        }
-        return answer;
-    }
-
-    strs.forEach(el => {
-        if (obj.get(countStr(el)) === undefined) obj.set(countStr(el), [el])
-        else obj.set(countStr(el), [...obj.get(countStr(el)), el])
+    strs.forEach((el, idx, org) => {
+        let sorted = el.split("").sort().join("");
+        if (obj.has(sorted)) obj.set(sorted, [...obj.get(sorted), el]);
+        else obj.set(sorted, [el]);
     })
 
-    return Array.from(obj.values());
+    return Array.from(obj.values())
 };
