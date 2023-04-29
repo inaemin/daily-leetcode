b3ffa4482b9153011314bace734a0f2c2859e31b
inaemin <97337038+inaemin@users.noreply.github.com>
2023-04-30 01:34:46 +0900

    Time: 121 ms (100.00%) | Memory: 48.1 MB (100.00%) - LeetSync

---
+++ find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} A
+ * @param {number[]} B
+ * @return {number[]}
+ */
+var findThePrefixCommonArray = function(A, B) {
+    const setA = new Set();
+    const C = [];
+    for (let i=0; i<A.length; i++) {
+        setA.add(A[i]);
+        let cnt = 0;
+        for (let j=0; j<=i; j++) {
+            if (setA.has(B[j])) cnt += 1;
+        }
+        C[i] = cnt;
+    }
+    
+    return C;
+};
