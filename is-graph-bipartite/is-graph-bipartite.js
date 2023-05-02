bc1ad2346025a0736fbc3f033090d7111ad5bc2e
inaemin <97337038+inaemin@users.noreply.github.com>
2023-05-02 21:35:51 +0900

    Time: 68 ms (57.67%) | Memory: 45 MB (52.15%) - LeetSync

--- is-graph-bipartite/is-graph-bipartite.js
+++ is-graph-bipartite/is-graph-bipartite.js
@@ -5,20 +5,17 @@
 var isBipartite = function(graph) {
     const n = graph.length
     const color = Array(n).fill(0);
-    const visited = new Set();
     for (let i=0; i<graph.length; i++) {
-        if (!visited.has(i)) {
+        if (color[i] === 0) {
             const stack = [];
             stack.push(i);
-            visited.add(i);
             color[i] = 1;
             while (stack.length) {
                 let curr = stack.pop();
                 let connects = graph[curr];
                 let next_color = color[curr] * -1;
                 for (let connect of connects) {
-                    if (!visited.has(connect)) {
-                        visited.add(connect);
+                    if (color[connect] === 0) {
                         stack.push(connect);
                         color[connect] = next_color
                     } else {
