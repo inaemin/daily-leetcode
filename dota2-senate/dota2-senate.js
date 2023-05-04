1586c4b9318a8c48ab3645de843419ad76a76a0e
inaemin <97337038+inaemin@users.noreply.github.com>
2023-05-05 01:54:56 +0900

    Time: 73 ms (71.70%) | Memory: 45.2 MB (92.45%) - LeetSync

--- dota2-senate/dota2-senate.js
+++ dota2-senate/dota2-senate.js
@@ -3,22 +3,16 @@
  * @return {string}
  */
 var predictPartyVictory = function(senate) {
-    const radiant = [];
-    const dire = [];
-    const n = senate.length
-    for (let i=0; i<n; i++) {
-        senate[i] === "R" ? radiant.push(i) : dire.push(i);
-    }
-
-    while (radiant.length && dire.length) {
-        if (radiant[0] < dire[0]) {
-            dire.shift();
-            radiant.push(radiant.shift() + n)
-        } else if (radiant[0] > dire[0]) {
-            radiant.shift();
-            dire.push(dire.shift() + n)
+    const stack = [];
+    const senate_arr = senate.split("");
+    while (senate_arr.length) {
+        if (!stack.length || stack[stack.length-1] === senate_arr[0]) {
+            stack.push(senate_arr.shift());
+        } else {
+            senate_arr.shift();
+            senate_arr.push(stack.pop());
         }
     }
 
-    return radiant.length > dire.length ? "Radiant" : "Dire";
+    return stack[0] === "R" ? "Radiant" : "Dire";
 };
