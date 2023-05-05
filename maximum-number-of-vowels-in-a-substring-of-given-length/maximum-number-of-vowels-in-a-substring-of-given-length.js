7b3f7febbd614a20099f2d25591b4d6fc173fbd1
inaemin <97337038+inaemin@users.noreply.github.com>
2023-05-05 14:10:24 +0900

    Time: 102 ms (36.81%) | Memory: 48 MB (19.10%) - LeetSync

--- maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.js
+++ maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.js
@@ -6,19 +6,16 @@
 var maxVowels = function(s, k) {
     const vowel = ["a", "e", "i", "o", "u"];
     const s_arr = [...s];
-    const substring = [...s.slice(0, k)];
     let max = 0;
-    for (let s of substring) {
-        if (vowel.includes(s)) max += 1;
+    for (let i=0; i<k; i++) {
+        if (vowel.includes(s_arr[i])) max += 1;
     }
 
     let curr = max;
     for (let i=k; i<s_arr.length; i++) {
-        if (vowel.includes(substring[0])) curr -= 1;
+        if (vowel.includes(s_arr[i-k])) curr -= 1;
         if (vowel.includes(s_arr[i])) curr += 1;
         if (curr > max) max = curr;
-        substring.shift();
-        substring.push(s_arr[i]);
     }
 
     return max;
