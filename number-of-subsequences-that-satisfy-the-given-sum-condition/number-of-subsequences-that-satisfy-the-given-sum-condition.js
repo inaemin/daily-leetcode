a34eebccc157cb631da42069251aa952308c0fec
inaemin <97337038+inaemin@users.noreply.github.com>
2023-05-06 20:33:41 +0900

    Time: 216 ms (36.36%) | Memory: 56.9 MB (9.09%) - LeetSync

--- number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.js
+++ number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.js
@@ -3,16 +3,19 @@
  * @param {number} target
  * @return {number}
  */
-
-
 var numSubseq = function(nums, target) {
-    const mod = BigInt(10 ** 9 + 7);
-    let answer = BigInt(0);
+    const mod = 10 ** 9 + 7;
+    const pow = [1, 2];
+    for (let i=2; i<nums.length; i++) {
+        pow.push(pow[i-1] * 2 % mod)
+    }
+
+    let answer = 0;
     nums.sort((a, b) => a - b);
     let left = 0, right = nums.length - 1;
     while (left <= right) {
         if (nums[left] + nums[right] <= target) {
-            answer += BigInt(2) ** BigInt(right - left) % mod;
+            answer += pow[right-left];
             left += 1;
         } else {
             right -= 1;
