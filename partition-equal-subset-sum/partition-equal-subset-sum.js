24bf62db38d73f7be3cd663c77d9298d16d9686c
inaemin <97337038+inaemin@users.noreply.github.com>
2023-05-10 05:04:30 +0900

    Time: 113 ms (94.74%) | Memory: 43.3 MB (96.87%) - LeetSync

--- partition-equal-subset-sum/partition-equal-subset-sum.js
+++ partition-equal-subset-sum/partition-equal-subset-sum.js
@@ -5,17 +5,14 @@
 var canPartition = function(nums) {
     const total_sum = nums.reduce((r, e) => r + e, 0);
     if (total_sum % 2 === 1) return false;
-    const m = nums.length+1;
-    const n = total_sum/2 + 1;
-    const dp = [...Array(m)].map(el => new Array(n).fill(false));
-    for (let i=0; i<dp.length; i++) dp[i][0] = true;
-    for (let i=1; i<m; i++) {
-        for (let j=1; j<n; j++) {
-            if (dp[i-1][j] === true) dp[i][j] = true;
-            else if (nums[i-1] === j) dp[i][j] = true;
-            else if (j >= nums[i-1] && dp[i-1][j-nums[i-1]] === true) dp[i][j] = true;
+    const dp = Array(total_sum/2+1).fill(false);
+    dp[0] = true;
+    for (let num of nums) {
+        for (let i=dp.length-1; i>=0; i--) {
+            if (dp[i] === false && i >= num) {
+                dp[i] = dp[i-num]
+            }
         }
     }
-
-    return dp[m-1][n-1];
+    return dp[dp.length-1];
 };
