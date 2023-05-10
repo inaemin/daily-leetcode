798788af037c0fe66734afee474b4efd8e4fc9ee
inaemin <97337038+inaemin@users.noreply.github.com>
2023-05-10 21:03:52 +0900

    Time: 62 ms (39.39%) | Memory: 41.9 MB (64.62%) - LeetSync

--- spiral-matrix-ii/spiral-matrix-ii.js
+++ spiral-matrix-ii/spiral-matrix-ii.js
@@ -3,33 +3,25 @@
  * @return {number[][]}
  */
 var generateMatrix = function(n) {
-    const dp = [...Array(n)].map(el => new Array(n).fill(0));
-    const isEnded = (i, j) => {
-        if (j+1<n && dp[i][j+1] === 0) return [i, j+1, "right"];
-        else if (i+1<n && dp[i+1][j] === 0) return [i+1, j, "down"]
-        else if (j-1>=0 && dp[i][j-1] === 0) return [i, j-1, "left"]
-        else if (i-1>=0 && dp[i-1][j] === 0) return [i-1, j, "up"]
-        return "end"
-    }
-    const dfs = (i, j, direction, cnt, pre_i, pre_j) => {
-        if (i<0 || j<0 || i>=n || j>=n || dp[i][j] !== 0) return [cnt, pre_i, pre_j];
-        dp[i][j] = cnt
-        if (direction === "right") return dfs(i, j+1, direction, cnt+1, i, j);
-        if (direction === "down") return dfs(i+1, j, direction, cnt+1, i, j);
-        if (direction === "left") return dfs(i, j-1, direction, cnt+1, i, j);
-        if (direction === "up") return dfs(i-1, j, direction, cnt+1, i, j);
-    }
+    const matrix = [...Array(n)].map(el => new Array(n));
+    let top = 0, bottom = n-1, left = 0, right = n-1;
+    let num = 1;
+    while (top <= bottom && left <= right) {
+        // Traverse right
+        for (let i=left; i<=right; i++) matrix[top][i] = num++
+        top += 1;
 
-    dp[0][0] = 1;
-    let cnt = 2;
-    let i=0, j=0;
-    while (cnt <= n**2) {
-        const [next_i, next_j, direction] = isEnded(i, j);
-        const [next_cnt, x, y] = dfs(next_i, next_j, direction, cnt);
-        i = x;
-        j = y;
-        cnt = next_cnt;
-    }
+        // Traverse down
+        for (let i=top; i<=bottom; i++) matrix[i][right] = num++
+        right -= 1;
 
-    return dp
+        // Traverse left
+        for (let i=right; i>=left; i--) matrix[bottom][i] = num++
+        bottom -= 1;
+
+        // Traverse up
+        for (let i=bottom; i>=top; i--) matrix[i][left] = num++
+        left += 1;
+    }
+    return matrix;
 };
