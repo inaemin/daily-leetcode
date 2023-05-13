875d6f83cc04b83eaecad8fac5a34761ca9964a2
inaemin <97337038+inaemin@users.noreply.github.com>
2023-05-14 01:52:01 +0900

    Time: 153 ms (100.00%) | Memory: 51.2 MB (100.00%) - LeetSync

--- sum-in-a-matrix/sum-in-a-matrix.js
+++ sum-in-a-matrix/sum-in-a-matrix.js
@@ -6,9 +6,9 @@ var matrixSum = function(nums) {
     nums.forEach(el => el.sort((a, b) => a - b));
     let answer = 0;
     while (nums[0].length) {
-        const cand = []
-        for (let num of nums) cand.push(num.pop());
-        answer += Math.max(...cand);
+        let max = 0;
+        for (let num of nums) max = Math.max(max, num.pop());
+        answer += max;
     }
     
     return answer;
