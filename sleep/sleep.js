cca4cd38cdff2149b13cd39a0d65d2249da41089
inaemin <97337038+inaemin@users.noreply.github.com>
2023-05-15 16:08:56 +0900

    Time: 54 ms (83.97%) | Memory: 41 MB (99.28%) - LeetSync

--- sleep/sleep.js
+++ sleep/sleep.js
@@ -2,11 +2,7 @@
  * @param {number} millis
  */
 async function sleep(millis) {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve(millis)
-        }, millis);
-    })
+    return new Promise((resolve) => setTimeout(resolve, millis))
 }
 
 /** 
