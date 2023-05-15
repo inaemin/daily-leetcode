01ed977b7a78601d2d109b859b2ebfa8c3bc206e
inaemin <97337038+inaemin@users.noreply.github.com>
2023-05-15 20:45:45 +0900

    Time: 88 ms (12.07%) | Memory: 47.4 MB (9.68%) - LeetSync

--- remove-linked-list-elements/remove-linked-list-elements.js
+++ remove-linked-list-elements/remove-linked-list-elements.js
@@ -12,18 +12,31 @@
  */
 var removeElements = function(head, val) {
     // iterative solution
-    const dummy = new ListNode(0, head);
-    let prev = dummy
-    let curr = dummy.next
-    while (curr) {
-        if (curr.val === val) {
-            prev.next = curr.next;
-            curr = curr.next;
+    // const dummy = new ListNode(0, head);
+    // let prev = dummy
+    // let curr = dummy.next
+    // while (curr) {
+    //     if (curr.val === val) {
+    //         prev.next = curr.next;
+    //         curr = curr.next;
+    //     } else {
+    //         prev = prev.next;
+    //         curr = curr.next;
+    //     }
+    // }
+
+    // return dummy.next;
+
+    // recursive solution
+    const recur = (node) => {
+        if (!node) return null;
+        const nextNode = recur(node.next)
+        if (node.val === val) {
+            return nextNode;
         } else {
-            prev = prev.next;
-            curr = curr.next;
+            node.next = nextNode;
+            return node;
         }
     }
-
-    return dummy.next;
+    return recur(head);
 };
