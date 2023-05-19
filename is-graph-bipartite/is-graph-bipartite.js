2306dc4b1827be108bd9080ab6a1642a66ed0430
inaemin <97337038+inaemin@users.noreply.github.com>
2023-05-19 18:49:35 +0900

    Time: 78 ms (27.48%) | Memory: 44.9 MB (61.99%) - LeetSync

--- is-graph-bipartite/is-graph-bipartite.js
+++ is-graph-bipartite/is-graph-bipartite.js
@@ -5,19 +5,22 @@
 var isBipartite = function(graph) {
     const n = graph.length;
     const colors = Array(n).fill(0);
-    const dfs = (i, color) => {
-        if (colors[i] === color*-1) return false;
-        if (colors[i] === color) return true;
-        const stack = [...graph[i]];
-        colors[i] = color;
-        while (stack.length) {
-            if (!dfs(stack.pop(), color*-1)) return false;
-        }
-        return true;
-    }
-
     for (let i=0; i<n; i++) {
-        if (colors[i] === 0 && !dfs(i, 1)) return false;
+        if (colors[i] === 0 && graph.length) {
+            colors[i] = 1;
+            const stack = [i];
+            while (stack.length) {
+                const curr = stack.pop();
+                const neighbors = graph[curr];
+                const next_color = -1*colors[curr];
+                for (let neighbor of neighbors) {
+                    if (colors[neighbor] === 0) {
+                        stack.push(neighbor);
+                        colors[neighbor] = next_color;
+                    } else if (colors[neighbor]  === -1*next_color) return false;
+                }
+            }
+        }
     }
     return true;
 };
