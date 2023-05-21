bb5f8b5f93169ce8cc920b2a5adabbddf2dacdb1
inaemin <97337038+inaemin@users.noreply.github.com>
2023-05-21 15:52:17 +0900

    Time: 71 ms (80.25%) | Memory: 46.2 MB (64.56%) - LeetSync

--- json-deep-equal/json-deep-equal.js
+++ json-deep-equal/json-deep-equal.js
@@ -16,9 +16,7 @@ var areDeeplyEqual = function(o1, o2) {
         if (typeof o1[key[i]] !== "object" && typeof o2[key[i]] !== "object" && o1[key[i]] !== o2[key[i]]) return false;
         if (typeof o1[key[i]] === "object" && typeof o2[key[i]] === "object" && o1[key[i]] && o2[key[i]]) {
             const child_key = Object.keys(o2[key[i]]);
-            console.log(child_key)
             for (let j=0; j<child_key.length; j++) {
-                //console.log(o1[key[i]][child_key[j]], o2[key[i]][child_key[j]])
                 if (!areDeeplyEqual(o1[key[i]][child_key[j]], o2[key[i]][child_key[j]])) return false;
             }
         }
