9aab5e752bde1c6e80c37f1364c26488bd43f8af
inaemin <97337038+inaemin@users.noreply.github.com>
2023-05-21 14:21:00 +0900

    Time: 174 ms (100.00%) | Memory: 51.3 MB (100.00%) - LeetSync

--- lexicographically-smallest-palindrome/lexicographically-smallest-palindrome.js
+++ lexicographically-smallest-palindrome/lexicographically-smallest-palindrome.js
@@ -4,26 +4,13 @@
  */
 var makeSmallestPalindrome = function(s) {
     const arr = [...s];
-    // arr.length가 홀수라면
-    if (arr.length % 2 === 1) {
-        const middle = Math.floor(arr.length/2);
-        for (let i=1; i<=middle; i++) {
-            if (arr[middle-i] !== arr[middle+i]) {
-                if (arr[middle-i].charCodeAt(0) < arr[middle+i].charCodeAt(0)) arr[middle+i] = arr[middle-i];
-                else arr[middle-i] = arr[middle+i];
-            }
+    const middle = Math.floor(arr.length/2);
+    for (let i=0; i<=middle; i++) {
+        if (arr[i] !== arr[arr.length-1-i]) {
+            if (arr[i].charCodeAt(0) < arr[arr.length-1-i].charCodeAt(0)) arr[arr.length-1-i] = arr[i];
+            else arr[i] = arr[arr.length-1-i];
         }
     }
-    // arr.length가 짝수라면
-    else {
-        const p1 = arr.length/2 - 1;
-        const p2 = arr.length/2;
-        for (let i=0; i<=p1; i++) {
-            if (arr[p1-i] !== arr[p2+i]) {
-                if (arr[p1-i].charCodeAt(0) < arr[p2+i].charCodeAt(0)) arr[p2+i] = arr[p1-i];
-                else arr[p1-i] = arr[p2+i];
-            }
-        }
-    }
-    return arr.join("");
+
+    return arr.join("")
 };
