ca33b875d0cadea076c84f96561de4bb08ca3ead
inaemin <97337038+inaemin@users.noreply.github.com>
2023-05-22 20:16:42 +0900

    Time: 565 ms (97.75%) | Memory: 124.8 MB (28.15%) - LeetSync

--- lru-cache/lru-cache.js
+++ lru-cache/lru-cache.js
@@ -1,9 +1,19 @@
 /**
  * @param {number} capacity
  */
+function DoublyListNode(key, val, prev, next) {
+    this.key = key;
+    this.val = val;
+    this.prev = prev;
+    this.next = next;
+}
+
 var LRUCache = function(capacity) {
     this.map = new Map();
     this.size = capacity;
+    this.head = new DoublyListNode(null, null, null, null)
+    this.tail = new DoublyListNode(null, null, this.head, null)
+    this.head.next = this.tail;
 };
 
 /** 
@@ -11,13 +21,18 @@ var LRUCache = function(capacity) {
  * @return {number}
  */
 LRUCache.prototype.get = function(key) {
-    if (this.map.has(key)) {
-      const value = this.map.get(key);
-      this.map.delete(key);
-      this.map.set(key, value);
-      return value;
+    if (!this.map.has(key)) return -1;
+    else {
+        // 끝으로 업데이트하고
+        const node = this.map.get(key);
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+        node.prev = this.tail.prev;
+        this.tail.prev.next = node;
+        node.next = this.tail;
+        this.tail.prev = node;
+        return this.map.get(key).val;
     }
-    return -1;
 };
 
 /** 
@@ -26,14 +41,35 @@ LRUCache.prototype.get = function(key) {
  * @return {void}
  */
 LRUCache.prototype.put = function(key, value) {
-    // map에 이미 있는 경우
     if (this.map.has(key)) {
-      this.map.delete(key);
-    } else if (this.map.size === this.size) {
-    // map에 없지만 cache가 꽉 찬 경우
-      this.map.delete(this.map.keys().next().value); // 처음값 삭제
+        // node를 마지막으로 업데이트하고
+        const node = this.map.get(key);
+        node.val = value; // value update
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+        node.prev = this.tail.prev;
+        this.tail.prev.next = node;
+        node.next = this.tail;
+        this.tail.prev = node;
+        this.map.set(key, node);
     }
-    this.map.set(key, value);
+    else if (this.size === this.map.size) {
+        // map에 없으면서 이미 cache가 모두 차있다면
+        const newNode = new DoublyListNode(key, value, this.tail.prev, this.tail);
+        const leastUsedNode = this.head.next;
+        this.head.next = leastUsedNode.next;
+        leastUsedNode.next.prev = this.head;
+        this.tail.prev.next = newNode;
+        this.tail.prev = newNode;
+        this.map.delete(leastUsedNode.key);
+        this.map.set(key, newNode);
+    } else {
+        // map에 없으면서 cache에 공간이 있다면
+        const newNode = new DoublyListNode(key, value, this.tail.prev, this.tail)
+        this.tail.prev.next = newNode;
+        this.tail.prev = newNode;
+        this.map.set(key, newNode);
+    }
 };
 
 /** 
