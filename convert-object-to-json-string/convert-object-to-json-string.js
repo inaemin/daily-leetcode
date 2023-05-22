07178c4f3e312576cdd6b89899b46bbaa05a67f0
inaemin <97337038+inaemin@users.noreply.github.com>
2023-05-22 13:03:12 +0900

    Time: 54 ms (96.97%) | Memory: 46 MB (67.90%) - LeetSync

--- convert-object-to-json-string/convert-object-to-json-string.js
+++ convert-object-to-json-string/convert-object-to-json-string.js
@@ -9,14 +9,8 @@ var jsonStringify = function(object) {
         let answer = `[`;
         if (!object.length) return answer + `]`;
         for (let i=0; i<object.length; i++) {
-            let value = object[i];
-            if (typeof value === "string") value = `"${value}"`
-            if (typeof value === "object") value = jsonStringify(value);
-            if (i === object.length-1) {
-                answer += `${value}]`
-            } else {
-                answer += `${value},`
-            }
+            let value = jsonStringify(object[i]);
+            answer += i === object.length-1 ? `${value}]` : `${value},`;
         }
         return answer;
     }
@@ -25,14 +19,8 @@ var jsonStringify = function(object) {
         const keys = Object.keys(object);
         if (!keys.length) return answer + `}`
         for (let i=0; i<keys.length; i++) {
-            let value = object[keys[i]];
-            if (typeof value === "string") value = `"${value}"`
-            if (typeof value === "object") value = jsonStringify(value);
-            if (i === keys.length-1) {
-                answer += `"${keys[i]}":${value}}`
-            } else {
-                answer += `"${keys[i]}":${value},`
-            }
+            let value = jsonStringify(object[keys[i]]);
+            answer += i === keys.length-1 ? `"${keys[i]}":${value}}` : `"${keys[i]}":${value},`;
         }
         return answer;
     }
