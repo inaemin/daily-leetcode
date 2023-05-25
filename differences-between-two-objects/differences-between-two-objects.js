a48a28b97f4dfa310e56cd3a436bf3d03bea7cad
inaemin <97337038+inaemin@users.noreply.github.com>
2023-05-25 16:28:34 +0900

    Time: 53 ms (93.33%) | Memory: 43.7 MB (53.33%) - LeetSync

--- differences-between-two-objects/differences-between-two-objects.js
+++ differences-between-two-objects/differences-between-two-objects.js
@@ -4,7 +4,6 @@
  * @return {object}
  */
 function objDiff(obj1, obj2) {
-    const obj = {}
     // dfs
     const dfs = (v1, v2) => {
         if (typeof v1 !== "object" && typeof v2 !== "object" || typeof v1 !== typeof v2) return [v1, v2];
@@ -24,13 +23,5 @@ function objDiff(obj1, obj2) {
         }
     }
 
-    const keys = Object.keys(obj1);
-    for (let i=0; i<keys.length; i++) {
-        const key = keys[i];
-        if (obj1[key] !== undefined && obj2[key] !== undefined && obj1[key] !== obj2[key]) {
-            obj[key] = dfs(obj1[key], obj2[key]);
-        }
-    }
-
-    return obj;
+    return dfs(obj1, obj2) || {};
 };
