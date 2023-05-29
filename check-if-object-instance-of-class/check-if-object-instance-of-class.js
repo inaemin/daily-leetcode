629f09a3c891f24293da4092b5626fd4fb0bf3cf
inaemin <97337038+inaemin@users.noreply.github.com>
2023-05-29 15:59:11 +0900

    Time: 126 ms (6.73%) | Memory: 52.9 MB (28.73%) - LeetSync

--- check-if-object-instance-of-class/check-if-object-instance-of-class.js
+++ check-if-object-instance-of-class/check-if-object-instance-of-class.js
@@ -5,7 +5,8 @@
  */
 var checkIfInstanceOf = function(obj, classFunction) {
     if (obj === null || obj === undefined || typeof classFunction !== "function") return false
-    return Object(obj) instanceof classFunction
+    if (obj === classFunction.prototype) return true;
+    return checkIfInstanceOf(obj.__proto__, classFunction);
 };
 
 /**
