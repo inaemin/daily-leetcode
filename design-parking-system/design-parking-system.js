d9e245bc7e589d4fd59f81e556e9410ce0593134
inaemin <97337038+inaemin@users.noreply.github.com>
2023-05-29 14:05:14 +0900

    Time: 133 ms (44.91%) | Memory: 50.6 MB (18.86%) - LeetSync

---
+++ design-parking-system/design-parking-system.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} big
+ * @param {number} medium
+ * @param {number} small
+ */
+var ParkingSystem = function(big, medium, small) {
+    this.big = big;
+    this.medium = medium;
+    this.small = small;
+};
+
+/** 
+ * @param {number} carType
+ * @return {boolean}
+ */
+ParkingSystem.prototype.addCar = function(carType) {
+    if (carType === 1 && this.big) {this.big--; return true;}
+    else if (carType === 2 && this.medium) {this.medium--; return true;}
+    else if (carType === 3 && this.small) {this.small--; return true;}
+    return false;
+};
+
+/** 
+ * Your ParkingSystem object will be instantiated and called as such:
+ * var obj = new ParkingSystem(big, medium, small)
+ * var param_1 = obj.addCar(carType)
+ */
