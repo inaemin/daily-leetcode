3e37c3c26089b485b0f1ddf94051117016308f4d
inaemin <97337038+inaemin@users.noreply.github.com>
2023-06-02 19:54:01 +0900

    Time: 1195 ms (5.35%) | Memory: 86.5 MB (5.57%) - LeetSync

---
+++ design-underground-system/design-underground-system.js
@@ -0,0 +1,48 @@
+var UndergroundSystem = function() {
+    this.record = {};
+    this.passenger = {};
+};
+
+/** 
+ * @param {number} id 
+ * @param {string} stationName 
+ * @param {number} t
+ * @return {void}
+ */
+UndergroundSystem.prototype.checkIn = function(id, stationName, t) {
+    this.passenger[id] = [stationName, t];
+};
+
+/** 
+ * @param {number} id 
+ * @param {string} stationName 
+ * @param {number} t
+ * @return {void}
+ */
+UndergroundSystem.prototype.checkOut = function(id, stationName, end_time) {
+    const [startStation, start_time] = this.passenger[id];
+    const time = end_time - start_time;
+    if (!this.record[startStation]) this.record[startStation] = {[stationName]: [time]};
+    else if (!this.record[startStation][stationName]) this.record[startStation][stationName] = [time];
+    else if (this.record[startStation][stationName]) this.record[startStation][stationName].push(time);
+};
+
+/** 
+ * @param {string} startStation 
+ * @param {string} endStation
+ * @return {number}
+ */
+UndergroundSystem.prototype.getAverageTime = function(startStation, endStation) {
+    console.log(this.record[startStation], this.record[startStation][endStation])
+    const times = this.record[startStation][endStation];
+    const avg = times.reduce((res, ele) => res + ele, 0) / times.length;
+    return avg;
+};
+
+/** 
+ * Your UndergroundSystem object will be instantiated and called as such:
+ * var obj = new UndergroundSystem()
+ * obj.checkIn(id,stationName,t)
+ * obj.checkOut(id,stationName,t)
+ * var param_3 = obj.getAverageTime(startStation,endStation)
+ */
