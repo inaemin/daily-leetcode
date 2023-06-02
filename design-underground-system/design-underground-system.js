103a610b1f65d23ad7924903494fecaa655a69c2
inaemin <97337038+inaemin@users.noreply.github.com>
2023-06-02 20:34:28 +0900

    Time: 222 ms (86.72%) | Memory: 62.7 MB (10.28%) - LeetSync

--- design-underground-system/design-underground-system.js
+++ design-underground-system/design-underground-system.js
@@ -1,6 +1,6 @@
 var UndergroundSystem = function() {
     this.record = {};
-    this.passenger = {};
+    this.passenger = new Map();
 };
 
 /** 
@@ -10,7 +10,7 @@ var UndergroundSystem = function() {
  * @return {void}
  */
 UndergroundSystem.prototype.checkIn = function(id, stationName, t) {
-    this.passenger[id] = [stationName, t];
+    this.passenger.set(id, [stationName, t]);
 };
 
 /** 
@@ -20,7 +20,7 @@ UndergroundSystem.prototype.checkIn = function(id, stationName, t) {
  * @return {void}
  */
 UndergroundSystem.prototype.checkOut = function(id, stationName, end_time) {
-    const [startStation, start_time] = this.passenger[id];
+    const [startStation, start_time] = this.passenger.get(id);
     const time = end_time - start_time;
     if (!this.record[startStation]) this.record[startStation] = {[stationName]: [time]};
     else if (!this.record[startStation][stationName]) this.record[startStation][stationName] = [time];
@@ -33,7 +33,6 @@ UndergroundSystem.prototype.checkOut = function(id, stationName, end_time) {
  * @return {number}
  */
 UndergroundSystem.prototype.getAverageTime = function(startStation, endStation) {
-    console.log(this.record[startStation], this.record[startStation][endStation])
     const times = this.record[startStation][endStation];
     const avg = times.reduce((res, ele) => res + ele, 0) / times.length;
     return avg;
