8f0688f458b9be8c6419324638c9110d703e38fb
inaemin <97337038+inaemin@users.noreply.github.com>
2023-06-07 19:29:05 +0900

    Time: 120 ms (67.04%) | Memory: 49.1 MB (87.49%) - LeetSync

--- shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.js
+++ shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.js
@@ -3,31 +3,27 @@
  * @return {number}
  */
 var shortestPathBinaryMatrix = function(grid) {
-    let depth = 1;
-    const m = grid.length
-    const n = grid[0].length
-    const q = {[depth] : [[0,0]]};
-    if (grid[0][0] === 1) return -1;
-    if (m === 1 && n === 1) return depth;
+    // shortest path를 구하려면 bfs를 이용해야 함.
+    const m = grid.length;
+    const n = grid[0].length;
+    if (grid[0][0] === 1 || grid[m-1][n-1] === 1) return -1;
 
-    const bfs = (i, j, depth) => {
-        if (i === m-1 && j === n-1 && grid[i][j] === 0) return true;
-        if (i<0 || j<0 || i>=m || j>=n || grid[i][j] !== 0) return;
-        if (grid[i][j] === 0) {
-            if (q[depth] === undefined) q[depth] = [[i, j]];
-            else q[depth].push([i, j]);
+    const bfs = (i, j, step) => {
+        const directions = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]];
+        for (let dir of directions) {
+            const [moveX, moveY] = dir;
+            if (i+moveX >= 0 && i+moveX < m && j+moveY >= 0 && j+moveY < n && grid[i+moveX][j+moveY] === 0) {
+                grid[i+moveX][j+moveY] = step + 1;
+                queue.push([i+moveX, j+moveY, step + 1]);
+            }
         }
-        grid[i][j] = 1;
     }
-
-    while (q[depth] !== undefined) {
-        if (q[depth].some((pos) => {
-            const [i, j] = pos;
-            return bfs(i-1, j, depth+1) || bfs(i+1, j, depth+1) || bfs(i, j-1, depth+1) || bfs(i, j+1, depth+1)
-            || bfs(i-1, j-1, depth+1) || bfs(i-1, j+1, depth+1) || bfs(i+1, j-1, depth+1) || bfs(i+1, j+1, depth+1)
-        })) return depth+1;
-        depth += 1;
+    const queue = [[0, 0, 1]];
+    while (queue.length) {
+        const [x, y, step] = queue.shift();
+        grid[x][y] = step;
+        bfs(x, y, step);
     }
-
-    return -1;
+    if (!grid[m-1][n-1]) return -1;
+    return grid[m-1][n-1];
 };
