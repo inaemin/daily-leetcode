ada12bd8b95f467e5bdf0b55f0891f50cae5e6bc
inaemin <97337038+inaemin@users.noreply.github.com>
2023-06-08 15:04:29 +0900

    Time: 150 ms (76.19%) | Memory: 50.8 MB (50.48%) - LeetSync

--- extra-characters-in-a-string/extra-characters-in-a-string.js
+++ extra-characters-in-a-string/extra-characters-in-a-string.js
@@ -5,17 +5,16 @@
  */
 var minExtraChar = function(s, dictionary) {
     // knapsack?!
+    // 사전에 없는 letter의 개수를 dp에 저장
     const result = [0];
     for (let i=1; i<=s.length; i++) {
         result[i] = result[i-1] + 1;
         dictionary.forEach((word) => {
-            if (word.length <= i) {
-                if (word === s.slice(i-word.length, i)) {
-                    result[i] = Math.min(result[i-word.length], result[i]);
-                }
+            if (word.length <= i && word === s.slice(i-word.length, i)) {
+                result[i] = Math.min(result[i-word.length], result[i]);
             }
         })
     }
-    
+
     return result[result.length-1];
 };
