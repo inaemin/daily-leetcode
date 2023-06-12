3e2c5d0f588b81af5c881acd2d2025877967a25f
inaemin <97337038+inaemin@users.noreply.github.com>
2023-06-12 12:03:51 +0900

    Time: 62 ms (29.05%) | Memory: 42.2 MB (39.86%) - LeetSync

--- summary-ranges/summary-ranges.js
+++ summary-ranges/summary-ranges.js
@@ -3,25 +3,16 @@
  * @return {string[]}
  */
 var summaryRanges = function(nums) {
-    nums.push(null);
-    const answer = [];
-    let start = nums[0];
-    let prev = nums[0];
-    let end = nums[0];
-    for (let i=1; i<nums.length; i++) {
-        if (nums[i] === prev + 1) {
-            prev = nums[i];
-        } else {
-            end = nums[i-1];
-            if (start === end) answer.push(`${start}`);
-            else answer.push(`${start}->${end}`);
-            start = nums[i];
-            prev = nums[i];
-            end = nums[i];
+    const result = [];
+    let i=0, j=0;
+    while (j < nums.length) {
+        if (nums[j] + 1 === nums[j+1]) j++;
+        else {
+            if (i === j) result.push(`${nums[i]}`);
+            else result.push(`${nums[i]}->${nums[j]}`);
+            i = j+1;
+            j = j+1;
         }
     }
-    if (start === end) answer.push(`${start}`);
-    else answer.push(`${start}->${end}`);
-    answer.pop();
-    return answer;
+    return result;
 };
