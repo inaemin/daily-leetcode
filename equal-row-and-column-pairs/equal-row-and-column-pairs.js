7288ef1a411d94b358a859be4762ad7d39c50bfe
inaemin <97337038+inaemin@users.noreply.github.com>
2023-06-13 14:50:28 +0900

    Time: 153 ms (42.75%) | Memory: 51.4 MB (78.44%) - LeetSync

--- equal-row-and-column-pairs/equal-row-and-column-pairs.js
+++ equal-row-and-column-pairs/equal-row-and-column-pairs.js
@@ -4,21 +4,21 @@
  */
 var equalPairs = function(grid) {
     const n = grid.length;
-    const column = {};
+    const column = new Map();
     for (let j=0; j<n; j++) {
         const temp = []
         for (let i=1; i<n; i++) {
             temp.push(grid[i][j])
         }
-        if (column[grid[0][j]]) column[grid[0][j]].push(temp);
-        else column[grid[0][j]] = [temp];
+        if (column.has(grid[0][j])) column.set(grid[0][j], [...column.get(grid[0][j]), temp])
+        else column.set(grid[0][j], [temp])
     }
 
     let answer = 0;
     for (let i=0; i<n; i++) {
-        if (column[grid[i][0]]) {
+        if (column.has(grid[i][0])) {
             const [first, ...rest] = grid[i];
-            column[first].forEach((element) => {
+            column.get(first).forEach((element) => {
                 if (element.every((el, idx) => el === rest[idx])) answer++;
             })
         }
