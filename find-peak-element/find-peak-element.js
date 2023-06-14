0c116a162947f45288c5764ffb765e6c4632dcb6
inaemin <97337038+inaemin@users.noreply.github.com>
2023-06-14 17:27:41 +0900

    Time: 67 ms (13.39%) | Memory: 42.1 MB (50.58%) - LeetSync

---
+++ find-peak-element/find-peak-element.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function(nums) {
+    let left=0, right=nums.length-1;
+    while (left <= right) {
+        const middle = (left + right) / 2 | 0;
+        if (nums[middle] > (nums[middle-1] || -Infinity) && nums[middle] > (nums[middle+1] || -Infinity)) return middle;
+        else if (nums[middle] > (nums[middle-1] || -Infinity)) {
+            left = middle + 1;
+        } else {
+            right = middle - 1;
+        }
+    }
+};
