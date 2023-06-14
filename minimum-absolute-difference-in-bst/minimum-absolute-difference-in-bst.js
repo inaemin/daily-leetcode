634c2ad16cc5b3305194a08ac46e20aecb858fc3
inaemin <97337038+inaemin@users.noreply.github.com>
2023-06-14 13:14:14 +0900

    Time: 75 ms (72.70%) | Memory: 49 MB (22.19%) - LeetSync

---
+++ minimum-absolute-difference-in-bst/minimum-absolute-difference-in-bst.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var getMinimumDifference = function(root) {
+    const result = [];
+    const dfs = (node) => {
+        if (!node) return;
+        result.push(node.val);
+        dfs(node.left);
+        dfs(node.right);
+    }
+    dfs(root);
+    result.sort((a, b) => a - b);
+    let answer = Infinity;
+    for (let i=0; i<result.length-1; i++) {
+        answer = Math.min(answer, Math.abs(result[i] - result[i+1]));
+    }
+    return answer;
+};
