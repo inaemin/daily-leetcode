e86680e415e909b5c1970c6abbd02e4448f8f730
inaemin <97337038+inaemin@users.noreply.github.com>
2023-06-14 17:02:20 +0900

    Time: 52 ms (89.41%) | Memory: 41.7 MB (89.37%) - LeetSync

--- merge-sorted-array/merge-sorted-array.js
+++ merge-sorted-array/merge-sorted-array.js
@@ -8,18 +8,12 @@
 var merge = function(nums1, m, nums2, n) {
     let i=m-1, j=n-1, k=nums1.length-1;
     while (k>=0) {
-        if (i < 0) {
+        if (i < 0 || nums1[i] < nums2[j]) {
             nums1[k] = nums2[j];
             j--;
-        } else if (j < 0) {
+        } else if (j < 0 || nums1[i] >= nums2[j]) {
             nums1[k] = nums1[i];
             i--;
-        } else if (nums1[i] >= nums2[j]) {
-            [nums1[i], nums1[k]] = [nums1[k], nums1[i]];
-            i--;
-        } else {
-            nums1[k] = nums2[j];
-            j--;
         }
         k--;
     }
