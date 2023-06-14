a811eef2564c74fcd8b070d903518e06bfa08575
inaemin <97337038+inaemin@users.noreply.github.com>
2023-06-14 18:18:43 +0900

    Time: 104 ms (71.27%) | Memory: 49.6 MB (47.72%) - LeetSync

--- merge-intervals/merge-intervals.js
+++ merge-intervals/merge-intervals.js
@@ -7,16 +7,16 @@ var merge = function(intervals) {
         if (a[0] === b[0]) return a[1] - b[1];
         else return a[0] - b[0];
     })
-    const result = [];
-    let end_point = null;
-    for (let interval of intervals) {
-        const [min, max] = interval;
-        if (!result.length || min > end_point) {
-            result.push(interval);
+    
+    const result = [intervals[0]];
+    let end_point = intervals[0][1];
+    for (let i=1; i<intervals.length; i++) {
+        const [min, max] = intervals[i];
+        if (min > end_point) {
+            result.push(intervals[i]);
             end_point = max;
         } else if (min <= end_point) {
-            const [min_point, max_point] = result[result.length-1];
-            result[result.length-1] = [Math.min(min_point, min), Math.max(max_point, max)];
+            result[result.length-1][1] = Math.max(end_point, max);
             end_point = result[result.length-1][1];
         }
     }
