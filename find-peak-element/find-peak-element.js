4fa9cfc090dec6cb72c3e8f46070b80da9a1debc
inaemin <97337038+inaemin@users.noreply.github.com>
2023-06-14 17:30:31 +0900

    Time: 69 ms (8.19%) | Memory: 42.2 MB (39.17%) - LeetSync

--- find-peak-element/find-peak-element.js
+++ find-peak-element/find-peak-element.js
@@ -5,7 +5,7 @@
 var findPeakElement = function(nums) {
     let left=0, right=nums.length-1;
     while (left <= right) {
-        const middle = (left + right) / 2 | 0;
+        const middle = Math.floor((left+right)/2);
         if (nums[middle] > (nums[middle-1] || -Infinity) && nums[middle] > (nums[middle+1] || -Infinity)) return middle;
         else if (nums[middle] > (nums[middle-1] || -Infinity)) {
             left = middle + 1;
