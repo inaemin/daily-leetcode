729a17357e491599bd08a809c35b31859052a87a
inaemin <97337038+inaemin@users.noreply.github.com>
2023-06-20 15:53:53 +0900

    Time: 346 ms (89.57%) | Memory: 89.2 MB (34.97%) - LeetSync

--- k-radius-subarray-averages/k-radius-subarray-averages.js
+++ k-radius-subarray-averages/k-radius-subarray-averages.js
@@ -5,16 +5,17 @@
  */
 var getAverages = function(nums, k) {
     if (k === 0) return nums;
-    const result = Array(nums.length).fill(-1);
-    if (2*k+1 > nums.length) return result; // 최소개수가 nums 길이보다 크다면
-    const window = [...nums.slice(0, 2*k+1)];
-    let sum = window.reduce((r, e) => r + e, 0);
-    result[k] = sum / (2*k+1) | 0;
-    for (let i=2*k+1; i<nums.length; i++) {
-        window.push(nums[i]);
+    const n = nums.length;
+    const result = Array(n).fill(-1);
+    let sum = 0;
+    for (let i=0; i<n; i++) {
         sum += nums[i];
-        sum -= window.shift();
-        result[i-k] = sum / (2*k+1) | 0;
+        if (i < 2*k) continue;
+        else if (i === 2*k) result[i-k] = sum / (2*k+1) | 0;
+        else if (i > 2*k) {
+            sum -= nums[i-2*k-1];
+            result[i-k] = sum / (2*k+1) | 0;
+        }
     }
 
     return result;
