b7d136c23d582349c06509bd645e4011fa9dcd97
inaemin <97337038+inaemin@users.noreply.github.com>
2023-06-24 09:25:07 +0900

    Time: 250 ms (5.75%) | Memory: 50.9 MB (5.75%) - LeetSync

--- shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.js
+++ shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.js
@@ -14,10 +14,12 @@ var findUnsortedSubarray = function(nums) {
           if (max === null) max = i;
           else if (nums[i] >= nums[max]) max = i;
       }
+      //if (nums[max] === nums[nums.length-1]) max = nums.length-1;
       if ((nums[i-1] || -Infinity) >= nums[i] && nums[i] <= (nums[i+1] || Infinity)) {
           if (min === null) min = i;
           else if (nums[i] <= nums[min]) min = i;
       }
+      //if (nums[min] === nums[nums.length-1]) min = nums.length-1;
   }
 
   if (status) return 0; // 이미 sorting되어 있는 경우.
@@ -38,5 +40,6 @@ var findUnsortedSubarray = function(nums) {
           break;
       }
   }
+  console.log(min, max, start, end)
   return end - start + 1;
 };
