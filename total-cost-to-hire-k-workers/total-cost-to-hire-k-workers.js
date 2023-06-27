c2ebe8317e996016c96842283fe3dfa64b12c85a
inaemin <97337038+inaemin@users.noreply.github.com>
2023-06-27 20:38:50 +0900

    Time: 372 ms (18.37%) | Memory: 91.2 MB (10.20%) - LeetSync

--- total-cost-to-hire-k-workers/total-cost-to-hire-k-workers.js
+++ total-cost-to-hire-k-workers/total-cost-to-hire-k-workers.js
@@ -8,24 +8,41 @@ var totalCost = function(costs, k, candidates) {
     let answer = 0;
     const left = new MinPriorityQueue();
     const right = new MinPriorityQueue();
+    let left_ps = 0, right_ps = costs.length-1;
     for (let i=0; i<candidates; i++) {
-        if (!costs.length) break;
-        if (costs.length) left.enqueue(costs.shift());
-        if (costs.length) right.enqueue(costs.pop());
+        if (left_ps <= right_ps) {
+            left.enqueue(costs[left_ps]);
+            left_ps++;
+            if (left_ps <= right_ps) {
+                right.enqueue(costs[right_ps]);
+                right_ps--;
+            }
+        }
     }
     for (let i=0; i<k; i++) {
+        let status;
         if (left.isEmpty()) {
+            status = "right"
             answer += right.dequeue().element;
-            if (costs.length) right.enqueue(costs.pop());
         } else if (right.isEmpty()) {
+            status = "left"
             answer += left.dequeue().element;
-            if (costs.length) left.enqueue(costs.shift());
-        } else if (left.front().element <= right.front().element) {
-            answer += left.dequeue().element;
-            if (costs.length) left.enqueue(costs.shift());
         } else if (left.front().element > right.front().element) {
+            status = "right"
             answer += right.dequeue().element;
-            if (costs.length) right.enqueue(costs.pop());
+        } else if (left.front().element <= right.front().element) {
+            status = "left"
+            answer += left.dequeue().element;
+        }
+
+        if (left_ps <= right_ps) {
+            if (status === "right") {
+                right.enqueue(costs[right_ps]);
+                right_ps--;
+            } else if (status === "left") {
+                left.enqueue(costs[left_ps]);
+                left_ps++;
+            }
         }
     }
 
