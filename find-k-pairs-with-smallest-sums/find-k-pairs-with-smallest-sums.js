a566f933f14abec88d6046623beac331cf685587
inaemin <97337038+inaemin@users.noreply.github.com>
2023-06-28 09:02:48 +0900

    Time: 565 ms (33.33%) | Memory: 92.8 MB (26.04%) - LeetSync

--- find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.js
+++ find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.js
@@ -6,23 +6,21 @@
  */
 var kSmallestPairs = function(nums1, nums2, k) {
     const heap = new MaxPriorityQueue({priority: (ele) => ele.val});
-    let limit = Infinity;
     for (let i=0; i<nums1.length; i++) {
         for (let j=0; j<nums2.length; j++) {
             const ele = {
                 element: [nums1[i], nums2[j]],
                 val: nums1[i] + nums2[j]
             }
-            if (ele.val > limit) break;
-            if (ele.val <= limit) heap.enqueue(ele);
-            if (heap.size() > k) {
-                limit = heap.dequeue().element.val;
-            }
+            if (heap.size() === k && ele.val > heap.front().element.val) break;
+            heap.enqueue(ele);
+            if (heap.size() > k) heap.dequeue();
         }
     }
-    const result = heap.toArray().reduce((r, e) => {
-        r.unshift(e.element.element);
-        return r;
-    }, [])
+
+    const result = [];
+    while (heap.size()) {
+        result.unshift(heap.dequeue().element.element)
+    }
     return result;
 };
