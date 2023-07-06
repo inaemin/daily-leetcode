3e95b09f0bfbe1b23bc1c3bf15dcf570602a3eed
inaemin <97337038+inaemin@users.noreply.github.com>
2023-07-06 22:59:42 +0900

    Time: 61 ms (85.63%) | Memory: 46.1 MB (66.14%) - LeetSync

--- minimum-size-subarray-sum/minimum-size-subarray-sum.js
+++ minimum-size-subarray-sum/minimum-size-subarray-sum.js
@@ -4,18 +4,19 @@
  * @return {number}
  */
 var minSubArrayLen = function(target, nums) {
-    let answer = Infinity
-    let i=0;
-    let j=0;
-    let total = 0;
-    while (j < nums.length) {
-        total += nums[j];
-        while (total >= target) {
-            answer = Math.min(answer, j-i+1);
-            total -= nums[i];
-            i++
+    let start = 0, end = 0;
+    let sum = nums[0];
+    let answer = Infinity;
+    while (start < nums.length && end < nums.length) {
+        if (sum >= target) {
+            answer = Math.min(answer, end - start + 1);
+            sum -= nums[start];
+            start++;
         }
-        j++
+        else {
+            end++;
+            sum += nums[end];
+        }
     }
     return answer === Infinity ? 0 : answer;
 };
