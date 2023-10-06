4cc70cdd1f946f88731fe0166f187aee661c9765
inaemin <97337038+inaemin@users.noreply.github.com>
2023-10-06 16:11:47 +0900

    Time: 50 ms (91.43%) | Memory: 45.1 MB (5.89%) - LeetSync

--- 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.js
+++ 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.js
@@ -4,11 +4,15 @@
  * @return {number}
  */
 var countPairs = function(nums, target) {
+    nums.sort((a, b) => a - b);
+
     let answer = 0;
     for (let i=0; i<nums.length-1; i++) {
         for (let j=i+1; j<nums.length; j++) {
             if (nums[i] + nums[j] < target) {
                 answer++;
+            } else {
+                break;
             }
         }
     }
