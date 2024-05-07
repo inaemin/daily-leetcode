73f50bb082dfcaf3cf2030749768ca0dadca085d
inaemin <97337038+inaemin@users.noreply.github.com>
2024-05-07 22:03:12 +0900

    Time: 185 ms (92.80%) | Memory: 61.5 MB (72.80%) - LeetSync

--- 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.js
+++ 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.js
@@ -11,30 +11,20 @@
  */
 var doubleIt = function(head) {
     const arr = [];
-    const dfs = (head) => {
-        if (!head.next) {
-            arr.push(head.val)
-            return;
-        }
-        arr.push(head.val);
-        dfs(head.next);
+    let current = head;
+    let carry = 0;
+    while (current !== null) {
+        arr.push(current.val);
+        current = current.next;
     }
-    dfs(head)
-    const num_arr = [0];
-    for (let i=arr.length-1; i>=0; i--) {
-        let temp = num_arr[0]
-        num_arr[0] = (temp + arr[i] * 2) % 10;
-        num_arr.unshift((temp + arr[i] * 2) / 10 | 0);
+    current = null;
+    while (arr.length) {
+        const newValue = arr.pop() * 2 + carry;
+        carry = newValue / 10 | 0;
+        current = new ListNode(newValue % 10, current);
     }
-    while (num_arr[0] === 0) {
-        num_arr.shift()
+    if (carry) {
+        current = new ListNode(carry, current)
     }
-    console.log(num_arr)
-    const ans = new ListNode(num_arr?.[0] || 0);
-    let num_head = ans;
-    for (let i=0; i<num_arr.length-1; i++) {
-        num_head.next = new ListNode(num_arr[i+1])
-        num_head = num_head.next;
-    }
-    return ans;
+    return current;
 };
