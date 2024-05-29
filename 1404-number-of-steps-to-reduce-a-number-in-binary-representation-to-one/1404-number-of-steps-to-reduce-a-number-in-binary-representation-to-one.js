74da0a66964f8a2c2bac5ea5f472090cd4a0f8cf
inaemin <97337038+inaemin@users.noreply.github.com>
2024-05-29 17:48:05 +0900

    Time: 70 ms (5.56%), Space: 54.7 MB (7.41%) - LeetHub

--- 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.js
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.js
@@ -6,29 +6,31 @@ var numSteps = function(s) {
     let cnt = 0;
     while (s.length > 1) {
         if (s.endsWith('0')) {
+            // 문자열이 '0'으로 끝나면 마지막 비트를 제거
             s = s.slice(0, -1);
         } else {
-            let newS = ""
-            let carry = '1'
-            for (let i=s.length-1; i>=0; i--) {
+            // 문자열이 '1'으로 끝나면 1을 더하기
+            let newS = "";
+            let carry = '1';
+            for (let i = s.length - 1; i >= 0; i--) {
                 if (s[i] === '1' && carry === '1') {
-                    newS = '0' + newS
-                    carry = '1'
+                    newS = '0' + newS;
+                    carry = '1';
                 } else if (s[i] === '1' && carry === '0') {
                     newS = '1' + newS;
-                    carry = '0'
+                    carry = '0';
                 } else if (s[i] === '0' && carry === '1') {
                     newS = '1' + newS;
-                    carry = '0'
+                    carry = '0';
                 } else if (s[i] === '0' && carry === '0') {
                     newS = '0' + newS;
-                    carry = '0'
+                    carry = '0';
                 }
             }
             if (carry === '1') {
-                s = carry + newS;
+                s = '1' + newS;
             } else {
-                s = newS
+                s = newS;
             }
         }
         cnt++;
