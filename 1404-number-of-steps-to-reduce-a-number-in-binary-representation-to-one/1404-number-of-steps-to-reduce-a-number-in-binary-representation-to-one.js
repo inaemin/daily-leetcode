92fc0ae133706ac0bcaee96f64be0253a991165d
inaemin <97337038+inaemin@users.noreply.github.com>
2024-05-29 15:53:49 +0900

    Time: 72 ms (5.56%), Space: 57.1 MB (5.55%) - LeetHub

---
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var numSteps = function(s) {
+    let arr = s.split("").map(Number);
+    let cnt = 0;
+    while (arr.length > 1) {
+        if (arr.at(-1) === 0) {
+            arr = arr.slice(0, arr.length-1);
+            cnt++;
+        } else {
+            arr[arr.length-1] += 1;
+            let start = 0;
+            for (let i=arr.length-1; i>=0; i--) {
+                if (arr[i] > 1) {
+                    arr[i] = 0;
+                    if (i === 0) {
+                        start += 1;
+                    } else {
+                        arr[i-1] += 1;
+                    }
+                }
+            }
+            if (start) {
+                arr = [start, ...arr];
+            }
+            cnt++;
+        }
+    }
+
+    return cnt;
+};
