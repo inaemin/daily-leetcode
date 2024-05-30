9e03312fa2d0d2714da2fc545eeb941f7a75d567
inaemin <97337038+inaemin@users.noreply.github.com>
2024-05-31 01:47:26 +0900

    Time: 63 ms (14.29%), Space: 48.9 MB (85.71%) - LeetHub

--- 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.js
+++ 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.js
@@ -3,27 +3,18 @@
  * @return {number}
  */
 var countTriplets = function(arr) {
-    // xor 연산은 다르면 1, 같으면 0을 반환.
-    const dp = Array.from({length: arr.length}, () => Array.from({length: arr.length}, () => null))
-    for (let i=0; i<arr.length; i++) {
-        dp[i][i] = arr[i]
-        for (let j=i+1; j<arr.length; j++) {
-            dp[i][j] = dp[i][j-1] ^ arr[j];
-        }
+    const n = arr.length;
+    const xor = Array.from({length: n+1}, () => 0);
+    for (let i=0; i<n; i++) {
+        xor[i+1] = xor[i] ^ arr[i];
     }
-
     let result = 0;
-    for (let i=0; i<dp.length; i++) {
-        for (let j=i; j<dp.length; j++) {
-            const a = dp[i][j];
-            for (let m=j+1; m<dp.length; m++) {
-                const b = dp[j+1][m]
-                if (a === b) {
-                    result++;
-                }
+    for (let i=0; i<xor.length; i++) {
+        for (let j=i+1; j<xor.length; j++) {
+            if (xor[i] === xor[j+1]) {
+                result += j-i
             }
         }
     }
-
     return result;
 };
