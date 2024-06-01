353240489dd089497d105efc2ea6208218a4e00a
inaemin <97337038+inaemin@users.noreply.github.com>
2024-06-01 14:20:08 +0900

    Time: 61 ms (55.20%) | Memory: 49.5 MB (87.84%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.js
+++ 3379-score-of-a-string/score-of-a-string.js
@@ -3,10 +3,9 @@
  * @return {number}
  */
 var scoreOfString = function(s) {
-    const char = s.split("").map(el => el.charCodeAt())
     let answer = 0;
     for (let i=1; i<s.length; i++) {
-        answer += Math.abs(char[i] - char[i-1]);
+        answer += Math.abs(s.charCodeAt(i) - s.charCodeAt(i-1));
     }
 
     return answer;
