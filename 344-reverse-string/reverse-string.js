d11f932fd2d1171ed5af99bc5c24c33632825f2e
inaemin <97337038+inaemin@users.noreply.github.com>
2024-06-02 15:04:42 +0900

    Time: 82 ms (38.40%) | Memory: 56.9 MB (65.73%) - LeetSync

--- 344-reverse-string/reverse-string.js
+++ 344-reverse-string/reverse-string.js
@@ -3,10 +3,5 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-    for (let i=0; i<Math.floor(s.length/2); i++) {
-        let temp = s[i];
-        s[i] = s[s.length-1-i]
-        s[s.length-1-i] = temp;
-    }
-    return s;
+    return s.reverse()
 };
