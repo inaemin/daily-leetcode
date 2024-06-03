a21ca226602a0328e88af4fce21e3ffebb9adbf0
inaemin <97337038+inaemin@users.noreply.github.com>
2024-06-03 15:55:51 +0900

    Time: 53 ms (82.76%) | Memory: 52.1 MB (87.36%) - LeetSync

--- 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.js
+++ 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.js
@@ -4,11 +4,12 @@
  * @return {number}
  */
 var appendCharacters = function(s, t) {
-    let t_pointer = 0;
-    for (let s_pointer=0; s_pointer<s.length; s_pointer++) {
-        if (s[s_pointer] === t[t_pointer]) {
-            t_pointer++;
+    let i=0, j=0;
+    while (i<s.length && j<t.length) {
+        if (s[i] === t[j]) {
+            j++;
         }
+        i++;
     }
-    return t.length - t_pointer;
+    return t.length-j;
 };
