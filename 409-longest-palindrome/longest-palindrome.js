5593e538eeecbc8809e05bdbc3993d61802f531e
inaemin <97337038+inaemin@users.noreply.github.com>
2024-06-04 16:04:26 +0900

    Time: 46 ms (96.61%) | Memory: 51 MB (44.96%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.js
+++ 409-longest-palindrome/longest-palindrome.js
@@ -3,28 +3,16 @@
  * @return {number}
  */
 var longestPalindrome = function(s) {
-    const obj = {};
-    for (let i=0; i<s.length; i++) {
-        if (!obj[s[i]]) {
-            obj[s[i]] = 1;
-        } else {
-            obj[s[i]]++;
-        }
-    }
-
-    const keys = Object.keys(obj);
+    const set = new Set();
     let length = 0;
-    let isOdd = false;
-    for (let i=0; i<keys.length; i++) {
-        const cnt = obj[keys[i]];
-        if (Math.floor(cnt/2) > 0) {
-            length += Math.floor(cnt/2) * 2;
-            if (cnt % 2 === 1) {
-                isOdd = true;
-            }
+    for (let i=0; i<s.length; i++) {
+        if (set.has(s[i])) {
+            length += 2;
+            set.delete(s[i])
         } else {
-            isOdd = true;
+            set.add(s[i])
         }
     }
-    return isOdd ? length + 1 : length;
+    
+    return set.size > 0 ? length + 1 : length;
 };
