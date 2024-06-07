ee05bc644e0b5c8c6ef0ad68e928d63abc3b1b25
inaemin <97337038+inaemin@users.noreply.github.com>
2024-06-07 23:29:16 +0900

    Time: 145 ms (47.93%), Space: 71.2 MB (28.51%) - LeetHub

--- 0648-replace-words/0648-replace-words.js
+++ 0648-replace-words/0648-replace-words.js
@@ -1,14 +1,47 @@
+function Trie() {
+    this.trie = new Map();
+}
+
+Trie.prototype.insert = function(word) {
+    let pointer = this.trie;
+    for (let i=0; i<word.length; i++) {
+        if (!pointer.has(word[i])) {
+            pointer.set(word[i], new Map())
+        }
+        pointer = pointer.get(word[i])
+    }
+    pointer.set('isEnd', true);
+}
+
+Trie.prototype.startsWith = function(prefix) {
+    let pointer = this.trie;
+    let word = ''
+    for (let i=0; i<prefix.length; i++) {
+        if (pointer.has(prefix[i])) {
+            word += prefix[i]
+            pointer = pointer.get(prefix[i])
+            if (pointer.get('isEnd')) return word;
+        } else {
+            return prefix;
+        }
+    }
+    return word;
+}
+
 /**
  * @param {string[]} dictionary
  * @param {string} sentence
  * @return {string}
  */
 var replaceWords = function(dictionary, sentence) {
-    dictionary.sort((a, b) => a.length - b.length);
-    const words = sentence.split(" ").map((word) => {
-        const root = dictionary.find((dict) => word.startsWith(dict))
-        return root ? root : word;
-    });
-
-    return words.join(" ");
+    const trie = new Trie();
+    for (let word of dictionary) {
+        trie.insert(word);
+    }
+    const words = sentence.split(" ");
+    for (let i=0; i<words.length; i++) {
+        words[i] = trie.startsWith(words[i])
+        console.log(words[i])
+    }
+    return words.join(" ")
 };
