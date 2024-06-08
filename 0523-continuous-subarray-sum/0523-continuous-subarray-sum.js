78a295ff4bd37c8491d73f78ee81a39c7a3a3e9e
inaemin <97337038+inaemin@users.noreply.github.com>
2024-06-08 20:37:08 +0900

    Time: 126 ms (11.67%), Space: 70.6 MB (98.33%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.js
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.js
@@ -4,22 +4,17 @@
  * @return {boolean}
  */
 var checkSubarraySum = function(nums, k) {
-    const dividedByK = nums.reduce((a, e) => {
-        a.push(((a?.[a.length-1] || 0) + e) % k)
-        return a;
-    }, []);
-
+    let sum = 0;
     const hash = {};
-    for (let i=0; i<dividedByK.length; i++) {
-        if (i !== 0 && dividedByK[i] === 0) {
+    for (let i=0; i<nums.length; i++) {
+        sum = (sum + nums[i]) % k;
+        if (i !== 0 && sum === 0) {
             return true
-        } else if (hash[dividedByK[i]]) {
-            if (i - hash[dividedByK[i]][0] > 1) return true;
-            hash[dividedByK[i]].push(i)
-        } else {
-            hash[dividedByK[i]] = [i];
+        } else if (hash.hasOwnProperty(sum) && i - hash[sum] > 1) {
+            return true;
+        } else if (!hash.hasOwnProperty(sum)) {
+            hash[sum] = i;
         }
     }
-
     return false;
 };
