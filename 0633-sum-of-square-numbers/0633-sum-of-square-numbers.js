5fa33ad7c258476778409bd64b36adf3f85ca1da
inaemin <97337038+inaemin@users.noreply.github.com>
2024-06-17 16:20:30 +0900

    Time: 66 ms (29.45%), Space: 49.4 MB (68.49%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.js
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.js
@@ -4,16 +4,15 @@
  */
 var judgeSquareSum = function(c) {
     const root = Math.floor(c**0.5);
-    const arr = Array.from({length: root+1}, (_, i) => i**2);
     let i=0;
-    let j=arr.length-1;
+    let j=root;
     while (i<=j) {
-        if (arr[i] + arr[j] === c) {
+        if (i**2 + j**2 === c) {
             return true;
-        } else if (arr[i] + arr[j] < c) {
+        } else if (i**2 + j**2 < c) {
             // i 오른쪽 이동
             i++;
-        } else if (arr[i] + arr[j] > c) {
+        } else if (i**2 + j**2 > c) {
             // j 왼쪽 이동
             j--;
         }
