089ab00904e1828c43b8d8fb366e253c8bff89c2
inaemin <97337038+inaemin@users.noreply.github.com>
2024-06-24 05:45:13 +0900

    Time: 770 ms (5.17%), Space: 63.4 MB (26.72%) - LeetHub

---
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.js
@@ -0,0 +1,45 @@
+/**
+ * @param {number[]} nums
+ * @param {number} limit
+ * @return {number}
+ */
+var longestSubarray = function(nums, limit) {
+    // Monotonic Queue
+    // 단조 증가 큐 : array에 value를 추가할 때, 추가할 value보다 큰 element는 모조리 pop
+    // 여기는 ascendnig sorting.
+    // 단조 감소 큐 : array에 value를 추가할 때, 추가할 value보다 작은 element는 모조리 pop
+    // 여기는 descending soring.
+
+    const minQueue = [];
+    const maxQueue = [];
+    let left = 0;
+    let maxLen = 0;
+
+    for (let right=0; right<nums.length; right++) {
+        // minQueue 업데이트
+        while (minQueue.length && nums[minQueue.at(-1)] > nums[right]) {
+            minQueue.pop()
+        }
+        minQueue.push(right);
+        // maxQueue 업데이트
+        while (maxQueue.length && nums[maxQueue.at(-1)] < nums[right]) {
+            maxQueue.pop()
+        }
+        maxQueue.push(right);
+        // 현재 window가 조건을 만족하지 않는 경우, left를 이동
+        let diff = Math.abs(nums[maxQueue[0]] - nums[minQueue[0]])
+        while (diff > limit) {
+            left++;
+            if (maxQueue[0] < left) {
+                maxQueue.shift()
+            }
+            if (minQueue[0] < left) {
+                minQueue.shift()
+            }
+            diff = Math.abs(nums[maxQueue[0]] - nums[minQueue[0]])
+        }
+        maxLen = Math.max(maxLen, right-left+1)
+    }
+
+    return maxLen
+};
