124eb915d3fbeb2366d8f5e134757bf2520abf06
inaemin <97337038+inaemin@users.noreply.github.com>
2024-06-25 16:39:18 +0900

    Time: 50 ms (77.82%), Space: 48.9 MB (89.42%) - LeetHub

---
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var bstToGst = function(root) {
+    // 역 중위 순회 (Reverse In-order Traversal)
+    // 오른쪽 자식 -> 현재 노드 -> 왼쪽자식
+    let sum = 0;
+    const traverse = (node) => {
+        if (!node) return;
+        traverse(node.right)
+        sum += node.val;
+        node.val = sum;
+        if (node.left) {
+            traverse(node.left)
+        }
+        return node;
+    }
+    return traverse(root);
+};
