f75235ffedea57e7fbfebe1bb7206717e369c43c
inaemin <97337038+inaemin@users.noreply.github.com>
2024-06-27 10:10:57 +0900

    Time: 85 ms (63.12%), Space: 64.6 MB (43.74%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.js
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.js
@@ -3,20 +3,7 @@
  * @return {number}
  */
 var findCenter = function(edges) {
-    const hash = {};
-    let max_cnt = 0;
-    let max_node;
-    for (let [u, v] of edges) {
-        hash[u] = (hash?.[u] || 0) + 1;
-        if (hash[u] > max_cnt) {
-            max_cnt = hash[u];
-            max_node = u;
-        }
-        hash[v] = (hash?.[v] || 0) + 1;
-        if (hash[v] > max_cnt) {
-            max_cnt = hash[v];
-            max_node = v;
-        }
-    }
-    return max_node;
+    const edge1 = edges[0]
+    const edge2 = edges[1]
+    return edge2.includes(edge1[0]) ? edge1[0] : edge1[1]
 };
