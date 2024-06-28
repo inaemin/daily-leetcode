6aca80fa390de78dd359bfd80a3550f90661fd2c
inaemin <97337038+inaemin@users.noreply.github.com>
2024-06-28 11:05:23 +0900

    Time: 188 ms (34%), Space: 73.8 MB (68%) - LeetHub

--- 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.js
+++ 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.js
@@ -6,15 +6,15 @@
 var maximumImportance = function (n, roads) {
     // 최대 total importance
     // 도로연결이 많이 되어있을수록 importance가 커짐
-    const hash = {}
+    const nodes = Array(n).fill(0);
     for (let [u, v] of roads) {
-        hash[u] = (hash?.[u] || 0) + 1;
-        hash[v] = (hash?.[v] || 0) + 1; 
+        nodes[u]++;
+        nodes[v]++;
     }
-    const connect_road = Object.values(hash).sort((a, b) => b - a);
+    nodes.sort((a, b) => b - a);
     let max_sum = 0;
-    for (let i = 0; i < connect_road.length; i++) {
-        max_sum += connect_road[i] * (n - i)
+    for (let i = 0; i < nodes.length; i++) {
+        max_sum += nodes[i] * (n - i)
     }
     return max_sum;
 };
