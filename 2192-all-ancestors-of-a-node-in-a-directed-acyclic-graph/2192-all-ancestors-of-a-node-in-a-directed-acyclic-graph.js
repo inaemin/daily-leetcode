ca6f26f009114a58b51e3b67e5e365306f0be283
inaemin <97337038+inaemin@users.noreply.github.com>
2024-06-30 08:58:22 +0900

    Time: 459 ms (5.88%), Space: 128.4 MB (5.88%) - LeetHub

---
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @return {number[][]}
+ */
+var getAncestors = function(n, edges) {
+    const indegree = Array(n).fill(0);
+    const node = Array.from({ length: n }, () => new Array());
+    edges.forEach((edge) => {
+        const [x, y] = edge;
+        indegree[y] += 1;
+        node[x].push(y);
+    })
+
+    const queue = [];
+    const ancestor = Array.from({ length: n }, () => new Set());
+    for (let i=0; i<indegree.length; i++) {
+        if (indegree[i] === 0) {
+            queue.push(i);
+            indegree[i] -= 1;
+        }
+    }
+    while (queue.length) {
+        let next = queue.shift();
+        node[next].forEach(el => {
+            if (ancestor[next].size > 0) {
+                ancestor[next].forEach(an => ancestor[el].add(an));
+            }
+            ancestor[el].add(next);
+            indegree[el] -= 1;
+            if (indegree[el] === 0) {
+                queue.push(el);
+                indegree[el] -= 1;
+            }
+        });
+    }
+
+    return ancestor.map(el => Array.from(el).sort((a, b) => a - b));
+};
