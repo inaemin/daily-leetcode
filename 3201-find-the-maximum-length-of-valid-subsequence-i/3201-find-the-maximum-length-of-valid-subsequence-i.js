8d73db7255b37bab7481078365715705287c31e9
inaemin <97337038+inaemin@users.noreply.github.com>
2024-06-30 16:59:58 +0900

    Time: 115 ms (100%), Space: 73.7 MB (100%) - LeetHub

--- 3201-find-the-maximum-length-of-valid-subsequence-i/3201-find-the-maximum-length-of-valid-subsequence-i.js
+++ 3201-find-the-maximum-length-of-valid-subsequence-i/3201-find-the-maximum-length-of-valid-subsequence-i.js
@@ -2,34 +2,18 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maximumLength = function(nums) {
+var maximumLength = function (nums) {
     const new_nums = nums.map(el => el % 2);
-    let all_odd = 0
-    let all_double = 0
+    const counter = [0, 0, 0, 0] // [odd, event, toggle]
+    let toggle = new_nums[0];
     for (let num of new_nums) {
-        if (num === 0) {
-            all_double++;
-        } else {
-            all_odd++;
+        if (num === 1) counter[0]++;
+        else counter[1]++;
+        if (num === toggle) {
+            counter[2]++;
+            toggle ^= 1;
         }
     }
-    let odd_start = 0;
-    let double_start = 0;
-    let odd_start_idx = new_nums.findIndex(el => el === 1);
-    let double_start_idx = new_nums.findIndex(el => el === 0);
-    let next = 1;
-    for (let i=odd_start_idx; i<new_nums.length; i++) {
-        if (next === new_nums[i]) {
-            next ^= 1;
-            odd_start++;
-        }
-    }
-    next = 0;
-    for (let i=double_start_idx; i<new_nums.length; i++) {
-        if (next === new_nums[i]) {
-            next ^= 1;
-            double_start++;
-        }
-    } 
-    return Math.max(all_odd, all_double, odd_start, double_start)
+    
+    return Math.max(...counter);
 };
