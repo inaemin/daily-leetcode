0e2c1b440ea51e0f236dbf1e5e88e6ec95bb3ffe
inaemin <97337038+inaemin@users.noreply.github.com>
2024-07-04 08:59:55 +0900

    Time: 133 ms (6.12%), Space: 58.8 MB (20.41%) - LeetHub

--- 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/1616-minimum-difference-between-largest-and-smallest-value-in-three-moves.js
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/1616-minimum-difference-between-largest-and-smallest-value-in-three-moves.js
@@ -3,13 +3,14 @@
  * @return {number}
  */
 var minDifference = function(nums) {
+    const n = nums.length
     nums.sort((a, b) => a - b);
     if (nums.length < 5) return 0;
     
-    const case1 = nums[nums.length-4] - nums[0]
-    const case2 = nums[nums.length-3] - nums[1]
-    const case3 = nums[nums.length-2] - nums[2]
-    const case4 = nums[nums.length-1] - nums[3]
+    const case1 = nums[n-4] - nums[0]
+    const case2 = nums[n-3] - nums[1]
+    const case3 = nums[n-2] - nums[2]
+    const case4 = nums[n-1] - nums[3]
 
     return Math.min(case1, case2, case3, case4);
 };
