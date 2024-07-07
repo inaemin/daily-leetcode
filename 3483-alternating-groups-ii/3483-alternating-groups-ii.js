97323a9cd222cdf0dc5fa0bd1eb788b0aa3fb2b5
inaemin <97337038+inaemin@users.noreply.github.com>
2024-07-07 11:04:31 +0900

    Time: 84 ms (100%), Space: 61.3 MB (100%) - LeetHub

--- 3483-alternating-groups-ii/3483-alternating-groups-ii.js
+++ 3483-alternating-groups-ii/3483-alternating-groups-ii.js
@@ -6,7 +6,8 @@
 var numberOfAlternatingGroups = function(colors, k) {
     /*
     i와 i+1을 비교하여 다르면 1, 같으면 0으로 맵핑.
-    
+    맵을 돌면서 1인 i를 만나면 연속되는 1을 count
+    카운트한 게 k-1 이상이라면 output에 count - (k-1)을 더함.
      */
     const n = colors.length;
     const tiles = colors.map((color, idx) => color !== colors[(idx+1)%n] ? 1 : 0);
