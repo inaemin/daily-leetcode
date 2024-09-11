d8583195cf15a3efc8f9e32bea646f7b50f73746
inaemin <97337038+inaemin@users.noreply.github.com>
2024-09-11 22:47:22 +0900

    Time: 81 ms (19.84%), Space: 56.5 MB (30.48%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.js
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.js
@@ -1,57 +1,33 @@
+const isPalindrome = (start, end, str) => {
+    for (let i=0; i<(end-start)/2; i++) {
+        if (str[start+i] !== str[end-i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
 /**
  * @param {string} s
  * @return {boolean}
  */
 var validPalindrome = function(s) {
-    let isDeleted = false;
     let left = 0;
     let right = s.length-1;
-    let i=0;
-    while (i < s.length/2) {
+    for (let i=0; i<s.length/2; i++) {
         if (s[left+i] !== s[right-i]) {
-            if (isDeleted) {
-                return false;
+            if (s[left+1+i] === s[right-i] 
+            && s[left+i] === s[right-1-i]) {
+                return isPalindrome(left+1+i, right-i, s)
+                || isPalindrome(left+i, right-1-i, s);
+            } else if (s[left+1+i] === s[right-i]) {
+                return isPalindrome(left+1+i, right-i, s);
+            } else if (s[left+i] === s[right-1-i]) {
+                return isPalindrome(left+i, right-1-i, s);
             } else {
-                if (s[left+1+i] === s[right-i]
-                && s[left+i] === s[right-1-i]) {
-                    console.log("Hello")
-                    let j = i;
-                    let l = left, r = right;
-                    let isLeftPossible = true;
-                    let isRightPossible = true;
-                    while (j < s.length/2) {
-                        if (s[l+1+j] !== s[r-j]) {
-                            isLeftPossible = false;
-                            break;
-                        }
-                        j++;
-                    }
-                    j = i;
-                    while (j < s.length/2) {
-                        if (s[l+j] !== s[r-1-j]) {
-                            isRightPossible = false;
-                            break;
-                        }
-                        j++;
-                    }
-                    
-                    if (isLeftPossible || isRightPossible) {
-                        return true;
-                    } else {
-                        return false;
-                    }
-                } else if (s[left+1+i] === s[right-i]) {
-                    left++;
-                } else if (s[left+i] === s[right-1-i]) {
-                    right--;
-                } else {
-                    return false;
-                }
-                isDeleted = true;
+                return false;
             }
         }
-        i++;
     }
-
     return true;
 };
