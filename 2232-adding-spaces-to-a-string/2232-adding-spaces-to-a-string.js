c30f019a83d98a1c09ece5e97c1a32bedc5df7f6
inaemin <97337038+inaemin@users.noreply.github.com>
2024-12-03 12:04:23 +0900

    Time: 59 ms (89.09%), Space: 88.2 MB (64.52%) - LeetHub

--- 2232-adding-spaces-to-a-string/2232-adding-spaces-to-a-string.js
+++ 2232-adding-spaces-to-a-string/2232-adding-spaces-to-a-string.js
@@ -4,14 +4,10 @@
  * @return {string}
  */
 var addSpaces = function (s, spaces) {
-    let answer = ""
-    let pointer = 0;
-    for (let i = 0; i < s.length; i++) {
-        if (spaces[pointer] === i) {
-            answer += " "
-            pointer++;
-        }
-        answer += s[i];
+    let answer = s.slice(0, spaces[0])
+    for (let i=1; i<spaces.length; i++) {
+        answer += " "+ s.slice(spaces[i-1], spaces[i])
     }
+    answer += " " + s.slice(spaces.at(-1))
     return answer;
 };
