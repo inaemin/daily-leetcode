ec466f26f3d9e08dc3dbf4452ee65ccf92c79331
inaemin <97337038+inaemin@users.noreply.github.com>
2024-12-11 19:08:50 +0900

    Time: 147 ms (55.56%), Space: 63.2 MB (55.56%) - LeetHub

--- 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.js
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.js
@@ -4,12 +4,15 @@
  * @return {number}
  */
 var maximumBeauty = function(nums, k) {
+    // nums를 오름차순으로 정렬
     nums.sort((a, b) => a - b);
 
+    // 투포인터를 이용해 A[j] - A[i] <= 2*k를 만족하는 최대 길이를 구함.
     let start = 0;
     let end;
     let maxLength = 1;
     for (let i=1; i<nums.length; i++) {
+        // 길이가 maxLength보다 긴 경우에만 업데이트
         if (nums[i] - nums[start] <= 2*k && i - start + 1 > maxLength) {
             end = i;
             maxLength = end - start + 1;
