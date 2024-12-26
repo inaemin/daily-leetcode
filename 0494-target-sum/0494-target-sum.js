6e5a66e486cdff49659f6fab7f2a43c8f36aeae0
inaemin <97337038+inaemin@users.noreply.github.com>
2024-12-27 04:09:27 +0900

    Time: 8 ms (93.44%), Space: 49.7 MB (64.77%) - LeetHub

--- 0494-target-sum/0494-target-sum.js
+++ 0494-target-sum/0494-target-sum.js
@@ -4,22 +4,25 @@
  * @return {number}
  */
 var findTargetSumWays = function(nums, target) {
-    const memo = {};
-    const bt = (idx, sum=0) => {
-        const key = `${idx},${sum}`;
-        if (key in memo) {
-            return memo[key];
-        }
-        if (idx === nums.length) {
-            return sum === target ? 1 : 0;
-        }
+    const total = nums.reduce((a, b) => a + b, 0);
 
-        const add = bt(idx+1, sum + nums[idx]);
-        const subtract = bt(idx+1, sum - nums[idx]);
-
-        memo[key] = add + subtract;
-        return memo[key];
+    // +가 필요한 숫자들의 집합을 P, -가 필요한 숫자들의 집합을 N이라고 할때,
+    // P + N은 total이 되어야 하고, P - N은 target이 되어야 한다.
+    // 따라서 2P = total + target이고, P = (total + target) / 2가 됨.
+    // total + target은 무조건 짝수가 되어야 함.
+    if (Math.abs(target) > total || (target + total) % 2 !== 0) {
+        return 0;
     }
 
-    return bt(0);
+    const P = (total + target) / 2;
+    const dp = new Array(P+1).fill(0);
+    dp[0] = 1; // 합이 0인 경우는 공집합이므로 1가지
+
+    for (let num of nums) {
+        for (let i=P; i>=num; i--) {
+            dp[i] += dp[i-num];
+        }
+    }
+
+    return dp[P];
 };
