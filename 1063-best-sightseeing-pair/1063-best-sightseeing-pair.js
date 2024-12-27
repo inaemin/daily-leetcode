fb4cea1d18cac1428b7ed6069f98601290e0812a
inaemin <97337038+inaemin@users.noreply.github.com>
2024-12-28 02:12:29 +0900

    Time: 1 ms (93.33%), Space: 54.5 MB (73.33%) - LeetHub

--- 1063-best-sightseeing-pair/1063-best-sightseeing-pair.js
+++ 1063-best-sightseeing-pair/1063-best-sightseeing-pair.js
@@ -5,12 +5,11 @@
 var maxScoreSightseeingPair = function(values) {
     // values[i] + values[j] + i - j
     // (values[i] + i) + (values[j] - j)
-    const dp = values.map((el, idx) => ([el+idx, el-idx]));
-    let max_i = dp[0][0];
-    let max_sum = max_i + dp[1][1];
-    for (let k=1; k<dp.length; k++) {
-        max_sum = Math.max(max_sum, max_i + dp[k][1]);
-        max_i = Math.max(max_i, dp[k][0]);
+    let max_i = values[0];
+    let max_sum = max_i + (values[1] - 1);
+    for (let k=1; k<values.length; k++) {
+        max_sum = Math.max(max_sum, max_i + (values[k] - k));
+        max_i = Math.max(max_i, values[k] + k);
     }
     return max_sum;
 };
