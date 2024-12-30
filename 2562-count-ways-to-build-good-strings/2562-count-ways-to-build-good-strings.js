fc175550bc18bb675a2d54f8d2bf3288fddd5f3f
inaemin <97337038+inaemin@users.noreply.github.com>
2024-12-31 01:03:37 +0900

    Time: 55 ms (41.38%), Space: 61 MB (68.97%) - LeetHub

--- 2562-count-ways-to-build-good-strings/2562-count-ways-to-build-good-strings.js
+++ 2562-count-ways-to-build-good-strings/2562-count-ways-to-build-good-strings.js
@@ -10,14 +10,14 @@ var countGoodStrings = function(low, high, zero, one) {
     const dp = new Array(high).fill(0);
     dp[zero-1] += 1;
     dp[one-1] += 1;
-    for (let i=Math.min(zero, one); i<high; i++) {
+    let sum = 0;
+    for (let i=Math.min(zero, one)-1; i<high; i++) {
         dp[i] += (dp?.[i-zero] || 0) + (dp?.[i-one] || 0);
         dp[i] %= mod;
-    }
-    let sum = 0;
-    for (let i=low-1; i<high; i++) {
-        sum += dp[i];
-        sum %= mod;
+        if (i >= low-1 && i < high) {
+            sum += dp[i];
+            sum %= mod;
+        }
     }
     return sum;
 };
