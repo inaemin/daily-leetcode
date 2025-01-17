dca11730167d282a13db45952145d4ed5fe81618
inaemin <97337038+inaemin@users.noreply.github.com>
2025-01-18 00:22:17 +0900

    Time: 14 ms (25%), Space: 66 MB (50%) - LeetHub

--- 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.js
+++ 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.js
@@ -3,15 +3,15 @@
  * @return {boolean}
  */
 var doesValidArrayExist = function(derived) {
-    const original = [1];
-    for (let i=0; i<derived.length-1; i++) {
+    // 1 ^ 1 = 0
+    // 1 ^ 0 = 1
+    let original = 1;
+    for (let i=0; i<derived.length; i++) {
         if (derived[i] === 1) {
-            original.push(original[original.length-1] === 0 ? 1 : 0);
-        } else {
-            original.push(original[original.length-1] === 0 ? 0 : 1);
+            original ^= 1;
         }
     }
-    if (original[original.length-1] ^ original[0] === derived[derived.length-1])
+    if (original === 1)
         return true;
     return false;
 };
