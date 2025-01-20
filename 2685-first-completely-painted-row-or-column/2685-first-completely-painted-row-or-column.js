60878bc0f8d9a7f7ef5b087ac78842fac0c9ddea
inaemin <97337038+inaemin@users.noreply.github.com>
2025-01-20 20:06:34 +0900

    Time: 124 ms (58.33%), Space: 82 MB (62.5%) - LeetHub

---
+++ 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} arr
+ * @param {number[][]} mat
+ * @return {number}
+ */
+var firstCompleteIndex = function(arr, mat) {
+    const m = mat.length;
+    const n = mat[0].length;
+    const row = new Array(m).fill(0);
+    const col = new Array(n).fill(0);
+    const table = {}
+    for (let i=0; i<m; i++) {
+        for (let j=0; j<n; j++) {
+            table[mat[i][j]] = [i, j];
+        }
+    }
+
+    for (let k=0; k<arr.length; k++) {
+        const [i, j] = table[arr[k]];
+        row[i]++;
+        col[j]++;
+        if (row[i] >= n || col[j] >= m) {
+            return k
+        }
+    }
+
+
+};
