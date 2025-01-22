2556cd9552beb3cd695c3b26e585b6b07ea44ee2
inaemin <97337038+inaemin@users.noreply.github.com>
2025-01-22 15:51:23 +0900

    Time: 356 ms (57.89%), Space: 173.4 MB (5.26%) - LeetHub

--- 1876-map-of-highest-peak/1876-map-of-highest-peak.js
+++ 1876-map-of-highest-peak/1876-map-of-highest-peak.js
@@ -6,12 +6,13 @@ var highestPeak = function(isWater) {
     const m = isWater.length;
     const n = isWater[0].length;
     const queue = [];
-    const heights = Array.from({length: m}, () => new Array(n).fill(-1));
     for (let i=0; i<m; i++) {
         for (let j=0; j<n; j++) {
             if (isWater[i][j] === 1) {
                 queue.push([i, j, 0]);
-                heights[i][j] = 0;
+                isWater[i][j] = 0;
+            } else {
+                isWater[i][j] = -1;
             }
         }
     }
@@ -24,12 +25,12 @@ var highestPeak = function(isWater) {
         for (let k=0; k<4; k++) {
             const nx = i + dx[k];
             const ny = j + dy[k];
-            if (nx >= 0 && nx < m && ny >= 0 && ny < n && heights[nx][ny] === -1) {
+            if (nx >= 0 && nx < m && ny >= 0 && ny < n && isWater[nx][ny] === -1) {
                 queue.push([nx, ny, height + 1]);
-                heights[nx][ny] = height + 1;
+                isWater[nx][ny] = height + 1;
             }
         }
         idx++;
     }
-    return heights;
+    return isWater;
 };
