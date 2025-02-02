38f8cb088657e67261117efb2d237f611e5336ce
inaemin <97337038+inaemin@users.noreply.github.com>
2025-02-02 14:22:08 +0900

    Time: 1 ms (100%), Space: 52.3 MB (100%) - LeetHub

--- 3753-maximum-difference-between-even-and-odd-frequency-i/3753-maximum-difference-between-even-and-odd-frequency-i.js
+++ 3753-maximum-difference-between-even-and-odd-frequency-i/3753-maximum-difference-between-even-and-odd-frequency-i.js
@@ -8,16 +8,14 @@ var maxDifference = function(s) {
         let idx = s[i].charCodeAt() - 'a'.charCodeAt();
         alphabets[idx]++;
     }
-    let max_even = -1, min_even = 100, max_odd = -1;
+    let max_odd = -1, min_even = 100;
     for (let i=0; i<26; i++) {
         if (alphabets[i] === 0) continue;
         if (alphabets[i] % 2 === 1) {
             max_odd = Math.max(max_odd, alphabets[i]);
         } else {
             min_even = Math.min(min_even, alphabets[i]);
-            max_even = Math.max(max_even, alphabets[i]);
         }
     }
-    if (max_odd - max_even > max_odd - min_even) return max_odd - max_even;
     return max_odd - min_even;
 };
