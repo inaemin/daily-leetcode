65af138d83416feac6c33d3d37f6997522434dc5
inaemin <inaemin@gmail.com>
2026-07-12 20:48:04 +0900

    Add solution for Rank Transform of an Array

--- LeetCode/Rank-Transform-of-an-Array/Solution.js
+++ LeetCode/Rank-Transform-of-an-Array/Solution.js
@@ -3,21 +3,8 @@
 3 * @return {number[]}
 4 */
 5var arrayRankTransform = function(arr) {
-6    const n = arr.length;
-7    const idx_map = {};
-8    for (let i=0; i<n; i++) {
-9        if (!idx_map[arr[i]]) {
-10            idx_map[arr[i]] = [];
-11        }
-12        idx_map[arr[i]].push(i);
-13    }
-14    const keys = Object.keys(idx_map).sort((a, b) => a - b)
-15    const max_rank = keys.length;
-16    for (let i=0; i<max_rank; i++) {
-17        for (let idx of idx_map[keys[i]]) {
-18            arr[idx] = i+1;
-19        }
-20    }
-21
-22    return arr;
-23};
+6    const sorted = [...new Set(arr)].sort((a ,b) => a - b);
+7    const rankMap = new Map();
+8    sorted.forEach((v, i) => rankMap.set(v, i+1));
+9    return arr.map(v => rankMap.get(v));
+10};
